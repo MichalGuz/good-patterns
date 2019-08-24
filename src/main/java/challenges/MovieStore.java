d6b6c58e636b546af8438a4e4186ddece446f867
MichalGuz <michalguz@gmail.com>
2019-08-24 21:55:37 +0200

    Add new lists with items in the body of  method getMovies() in class MovieStore.

--- src/main/java/challenges/MovieStore.java
+++ src/main/java/challenges/MovieStore.java
@@ -1,11 +1,26 @@
 package challenges;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class MovieStore {
     public Map<String, List<String>> getMovies(){
+        List<String> drama = new ArrayList<>();
+        drama.add("Drama's title #1");
+        drama.add("Drama's title #2");
 
+        List<String> comedy = new ArrayList<>();
+        comedy.add("Comedy's title #1");
+        comedy.add("Comedy's title #2");
+
+        List<String> crime = new ArrayList<>();
+        crime.add("Crime's title #1");
+        crime.add("Crime's title #2");
+
+        List<String> horror = new ArrayList<>();
+        horror.add("Horror's title #1");
+        horror.add("Horror's title #2");
     }
 
 }
