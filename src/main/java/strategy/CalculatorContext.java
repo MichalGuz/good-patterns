1dc199f7a85310995af2734907de20c4dbd78890
MichalGuz <michalguz@gmail.com>
2019-08-26 23:59:19 +0200

    Create method set() in CalculatorContext class.

--- src/main/java/strategy/CalculatorContext.java
+++ src/main/java/strategy/CalculatorContext.java
@@ -3,4 +3,8 @@ package strategy;
 public class CalculatorContext {
     private CalculatorStrategyInterface calculator;
 
+    public void set(CalculatorStrategyInterface calculator){
+        this.calculator = calculator;
+    }
+
 }
