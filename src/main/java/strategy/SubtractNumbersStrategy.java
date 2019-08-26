1cc9c39032a5d8a268566734a5ff90e6e75ea1cb
MichalGuz <michalguz@gmail.com>
2019-08-26 23:52:06 +0200

    Add the implementation of interface AddNumbersStrategy in class SubtractNumbersStrategy.

--- src/main/java/strategy/SubtractNumbersStrategy.java
+++ src/main/java/strategy/SubtractNumbersStrategy.java
@@ -1,4 +1,7 @@
 package strategy;
 
-public class SubtractNumbersStrategy {
+public class SubtractNumbersStrategy implements CalculatorStrategyInterface {
+    public double calculate(double x, double y) {
+        return x - y;
+    }
 }
