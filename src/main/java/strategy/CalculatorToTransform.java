37fd6d2dc1a0d7a894f465a4f631e2acd3b930e6
MichalGuz <michalguz@gmail.com>
2019-08-26 23:47:48 +0200

    Create block of 'if' conditions in body of calculate() method.

--- src/main/java/strategy/CalculatorToTransform.java
+++ src/main/java/strategy/CalculatorToTransform.java
@@ -3,7 +3,15 @@ package strategy;
 public class CalculatorToTransform {
     public double calculate(double x, double y, String operator) {
         double result = 0;
-
+        if("add".equals(operator)){
+            result = x + y;
+        } else if ("subtract".equals(operator)){
+            result = x  + y;
+        } else if ("multiply".equals(operator)){
+            result = x * y;
+        } else if("divide".equals(operator)){
+            result = x / y;
+        }
         return result;
     }
 }
