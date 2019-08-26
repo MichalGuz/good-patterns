24b0a60aa03639c0ab97fb735f4a7dd6fb668026
MichalGuz <michalguz@gmail.com>
2019-08-26 23:46:29 +0200

    Create calculate() method."

--- src/main/java/strategy/CalculatorToTransform.java
+++ src/main/java/strategy/CalculatorToTransform.java
@@ -1,4 +1,9 @@
 package strategy;
 
 public class CalculatorToTransform {
+    public double calculate(double x, double y, String operator) {
+        double result = 0;
+
+        return result;
+    }
 }
