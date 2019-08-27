87cce65ba251d7200ebd9671db67122ed62d07fe
MichalGuz <michalguz@gmail.com>
2019-08-27 23:13:16 +0200

    Create main() method in class ClientCalculatorStrategy with new object of class CalculatorContext and local variable.

--- src/main/java/strategy/ClientCalculatorStrategy.java
+++ src/main/java/strategy/ClientCalculatorStrategy.java
@@ -1,4 +1,8 @@
 package strategy;
 
 public class ClientCalculatorStrategy {
+    public static void main(String[] args) {
+        CalculatorContext context = new CalculatorContext();
+        double result = 0;
+    }
 }
