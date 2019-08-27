d8780261cda9f4b041e10f45bc083d6889fedfae
MichalGuz <michalguz@gmail.com>
2019-08-27 23:16:40 +0200

    Use object 'context' to set new AddNumberStrategy().

--- src/main/java/strategy/ClientCalculatorStrategy.java
+++ src/main/java/strategy/ClientCalculatorStrategy.java
@@ -4,5 +4,7 @@ public class ClientCalculatorStrategy {
     public static void main(String[] args) {
         CalculatorContext context = new CalculatorContext();
         double result = 0;
-    }
+
+        context.set(new AddNumbersStrategy());
+     }
 }
