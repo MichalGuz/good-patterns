694dc4ff4000153ee1d41ce63be1b8a3c115823f
MichalGuz <michalguz@gmail.com>
2019-08-30 20:18:16 +0200

    Create class Customer with fields.

---
+++ src/main/java/order_service_solid/Customer.java
@@ -0,0 +1,9 @@
+package order_service_solid;
+
+public class Customer {
+    private String name;
+    private String surname;
+    private String customersNumber;
+    private String emailAdress;
+
+}
