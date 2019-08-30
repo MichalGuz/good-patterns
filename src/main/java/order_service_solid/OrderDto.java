17203cf946151d28b41afdef316cff9beb5bf17c
MichalGuz <michalguz@gmail.com>
2019-08-30 20:44:23 +0200

    Create class OrderDto, add fields, constructor and getters.

---
+++ src/main/java/order_service_solid/OrderDto.java
@@ -0,0 +1,19 @@
+package order_service_solid;
+
+public class OrderDto {
+    Customer customer;
+    boolean isOrdered;
+
+    public OrderDto(Customer customer, boolean isOrdered) {
+        this.customer = customer;
+        this.isOrdered = isOrdered;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public boolean isOrdered() {
+        return isOrdered;
+    }
+}
