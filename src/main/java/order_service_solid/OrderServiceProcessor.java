527ac3c305c3330ae567a03bbea21066bc818fb7
MichalGuz <michalguz@gmail.com>
2019-08-30 21:39:38 +0200

    Implement method order() in class OrderServiceProcessor.

--- src/main/java/order_service_solid/OrderServiceProcessor.java
+++ src/main/java/order_service_solid/OrderServiceProcessor.java
@@ -3,6 +3,12 @@ package order_service_solid;
 public class OrderServiceProcessor implements OrderService {
     @Override
     public boolean order(Customer customer, Product product, OrderData orderData){
+        System.out.println("Order number: " + orderData.getNumberOfOrder() + "\n"
+                + "Date of placed of order: " + orderData.getDateOfOrder() + "\n"
+                + "Purchaser: " + customer.getName() + " " + customer.getSurname() + ", ID of User: " + customer.getCustomersNumber() + ".\n"
+                + "Details of the order:\n" + "[Product ID] " + product.getProductId()
+                + "\n[Product] " + product.getName() + "\n[Price] " + product.getPrice()
+                + "\n[Quantity] " + orderData.getQuintityOfProducts() + "\n[Total price: ] " + orderData.getTotalPriceOfOrder());
         return true;
     }
 }
