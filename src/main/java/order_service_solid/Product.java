77e64c698fce1546b89f478d17781a1100ba3769
MichalGuz <michalguz@gmail.com>
2019-08-30 20:22:00 +0200

    Create constructor and getters in class Product.

--- src/main/java/order_service_solid/Product.java
+++ src/main/java/order_service_solid/Product.java
@@ -4,4 +4,16 @@ public class Product {
     private String name;
     private String productId;
 
+    public Product(String name, String productId) {
+        this.name = name;
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
 }
