1318f0624be98bfaa3a456649e3ea0478a4b0993
MichalGuz <michalguz@gmail.com>
2019-08-30 21:43:01 +0200

    Implement method createOrder() in class OrderRepositoryProcessor.

--- src/main/java/order_service_solid/OrderRepositoryProcessor.java
+++ src/main/java/order_service_solid/OrderRepositoryProcessor.java
@@ -3,6 +3,8 @@ package order_service_solid;
 public class OrderRepositoryProcessor implements OrderRepository {
     @Override
     public boolean createOrder(Customer customer, Product product, OrderData orderData){
+        System.out.println("Order number: " + orderData.getNumberOfOrder()
+                + " was saved in order repository");
         return true;
     }
 }
