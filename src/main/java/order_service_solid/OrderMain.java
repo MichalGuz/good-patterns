45de2abfd32acc7baee9ac18148eb3bd826ad787
MichalGuz <michalguz@gmail.com>
2019-08-30 21:44:39 +0200

    Create new object of classes Customer, Product, OrderData in main() method.

--- src/main/java/order_service_solid/OrderMain.java
+++ src/main/java/order_service_solid/OrderMain.java
@@ -4,5 +4,10 @@ import java.time.LocalDate;
 
 public class OrderMain {
     public static void main(String[] args) {
+        Customer customerOne = new Customer("John", "Smith", "00001", "johnsmith@mail.com");
+        Product product1 = new Product("watch", "01234567", 650.0);
+        OrderData orderData = new OrderData(LocalDate.of(2019, 8, 28), "0000001", 1, 650.0);
+
+
     }
 }
