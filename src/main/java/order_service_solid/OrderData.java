8cb19e61ec224bfde76fe39da45b5786cb932b2f
MichalGuz <michalguz@gmail.com>
2019-08-30 20:28:39 +0200

    Create constructor and getters in class OrderData.

--- src/main/java/order_service_solid/OrderData.java
+++ src/main/java/order_service_solid/OrderData.java
@@ -8,5 +8,28 @@ public class OrderData {
     int quintityOfProducts;
     Double totalPriceOfOrder;
 
+    public OrderData(LocalDate dateOfOrder, String numberOfOrder, int quintityOfProducts, Double totalPriceOfOrder) {
+        this.dateOfOrder = dateOfOrder;
+        this.numberOfOrder = numberOfOrder;
+        this.quintityOfProducts = quintityOfProducts;
+        this.totalPriceOfOrder = totalPriceOfOrder;
+    }
 
+    public LocalDate getDateOfOrder() {
+        return dateOfOrder;
+    }
+
+    public String getNumberOfOrder() {
+        return numberOfOrder;
+    }
+
+    public int getQuintityOfProducts() {
+        return quintityOfProducts;
+    }
+
+    public Double getTotalPriceOfOrder() {
+        return totalPriceOfOrder;
+    }
 }
+
+
