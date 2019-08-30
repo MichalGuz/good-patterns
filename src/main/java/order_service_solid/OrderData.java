64c511cb0b7fed442b7490547f17bd4242606603
MichalGuz <michalguz@gmail.com>
2019-08-30 20:26:44 +0200

    Create class OrderData with fields.

---
+++ src/main/java/order_service_solid/OrderData.java
@@ -0,0 +1,12 @@
+package order_service_solid;
+
+import java.time.LocalDate;
+
+public class OrderData {
+    LocalDate dateOfOrder;
+    String numberOfOrder;
+    int quintityOfProducts;
+    Double totalPriceOfOrder;
+
+
+}
