774c950f49ca40cf2ada8c3d0f3e5a5d3f442cea
MichalGuz <michalguz@gmail.com>
2019-08-30 21:15:41 +0200

    Implement method inform() in class OrderMailService.

--- src/main/java/order_service_solid/OrderMailService.java
+++ src/main/java/order_service_solid/OrderMailService.java
@@ -1,7 +1,8 @@
 package order_service_solid;
 
 public class OrderMailService implements InformationService {
+    @Override
     public void inform(Customer customer){
-
+        System.out.println("Send email with confirmation of order to: " + customer.getEmailAdress());
     }
 }
