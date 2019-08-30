557284873eadca04f7ed4f0ebb43517564ddc9b3
MichalGuz <michalguz@gmail.com>
2019-08-30 20:38:26 +0200

    Complete class ProductOrderProcessor: add fields and constructor.

--- src/main/java/order_service_solid/ProductOrderProcessor.java
+++ src/main/java/order_service_solid/ProductOrderProcessor.java
@@ -1,5 +1,13 @@
 package order_service_solid;
 
 public class ProductOrderProcessor {
+    private OrderService orderService;
+    private OrderRepository orderRepository;
+    private InformationService informationService;
 
+    public ProductOrderProcessor(final OrderService orderService, final OrderRepository orderRepository, final InformationService informationService) {
+        this.orderService = orderService;
+        this.orderRepository = orderRepository;
+        this.informationService = informationService;
+    }
 }
