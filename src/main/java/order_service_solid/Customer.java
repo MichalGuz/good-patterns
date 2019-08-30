8588e86989d95263d53e18c20c563f9bfd8e1b67
MichalGuz <michalguz@gmail.com>
2019-08-30 20:19:39 +0200

    Create constructor and getters in class Customer.

--- src/main/java/order_service_solid/Customer.java
+++ src/main/java/order_service_solid/Customer.java
@@ -6,4 +6,26 @@ public class Customer {
     private String customersNumber;
     private String emailAdress;
 
+    public Customer(String name, String surname, String customersNumber, String emailAdress) {
+        this.name = name;
+        this.surname = surname;
+        this.customersNumber = customersNumber;
+        this.emailAdress = emailAdress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getCustomersNumber() {
+        return customersNumber;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
 }
