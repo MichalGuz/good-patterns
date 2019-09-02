c7558677dabedd12ad15076da5eee26d67a077a7
MichalGuz <michalguz@gmail.com>
2019-09-02 23:51:33 +0200

    dd field 'Producer producer' and complete constructor of Product class and its get() methods.

--- src/main/java/easy_eats/Product.java
+++ src/main/java/easy_eats/Product.java
@@ -1,14 +1,20 @@
 package easy_eats;
 
 public class Product {
+    private Producer producer;
     private String productsName;
     private String productsId;
     private Double productsPrice;
 
-    public Product(String productsName, String productsId, Double productsPrice) {
+    public Product(Producer producer, String productsName, String productsId, Double productsPrice) {
+        this.producer = producer;
         this.productsName = productsName;
         this.productsId = productsId;
         this.productsPrice = productsPrice;
+    }
+
+    public Producer getProducer() {
+        return producer;
     }
 
     public String getProductsName() {
