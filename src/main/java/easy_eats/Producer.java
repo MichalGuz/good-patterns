dd68b3097d253128e8ad774e6c021e5ac479228e
MichalGuz <michalguz@gmail.com>
2019-09-02 19:26:14 +0200

    Create fields and constructor of class Producer.

--- src/main/java/easy_eats/Producer.java
+++ src/main/java/easy_eats/Producer.java
@@ -1,4 +1,15 @@
 package easy_eats;
 
 public class Producer {
+    private String name;
+    private String contact;
+    private String account;
+
+    public Producer(String name, String contact, String account) {
+        this.name = name;
+        this.contact = contact;
+        this.account = account;
+    }
+
+
 }
