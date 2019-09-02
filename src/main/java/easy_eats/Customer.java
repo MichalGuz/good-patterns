13386c0576613fe02cad1025d0379050050d157e
MichalGuz <michalguz@gmail.com>
2019-09-02 19:31:51 +0200

    Create constructor of Customer class and its get() methods.

--- src/main/java/easy_eats/Customer.java
+++ src/main/java/easy_eats/Customer.java
@@ -5,4 +5,27 @@ public class Customer {
     private String customersId;
     private String customersAdress;
     private String customersEmail;
+
+    public Customer(String customersName, String customersId, String customersAdress, String customersEmail) {
+        this.customersName = customersName;
+        this.customersId = customersId;
+        this.customersAdress = customersAdress;
+        this.customersEmail = customersEmail;
+    }
+
+    public String getCustomersName() {
+        return customersName;
+    }
+
+    public String getCustomersId() {
+        return customersId;
+    }
+
+    public String getCustomersAdress() {
+        return customersAdress;
+    }
+
+    public String getCustomersEmail() {
+        return customersEmail;
+    }
 }
