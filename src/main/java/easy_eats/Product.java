733938c6c20f08c2568ccc6c17ae492c092fca14
MichalGuz <michalguz@gmail.com>
2019-09-02 19:34:12 +0200

    Create constructor of Product class and its get() methods.

--- src/main/java/easy_eats/Product.java
+++ src/main/java/easy_eats/Product.java
@@ -4,4 +4,22 @@ public class Product {
     private String productsName;
     private String productsId;
     private Double productsPrice;
+
+    public Product(String productsName, String productsId, Double productsPrice) {
+        this.productsName = productsName;
+        this.productsId = productsId;
+        this.productsPrice = productsPrice;
+    }
+
+    public String getProductsName() {
+        return productsName;
+    }
+
+    public String getProductsId() {
+        return productsId;
+    }
+
+    public Double getProductsPrice() {
+        return productsPrice;
+    }
 }
