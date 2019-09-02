c1d2e54a870f4a360395c5b318f1268281a2ef0e
MichalGuz <michalguz@gmail.com>
2019-09-02 19:30:59 +0200

    Create Customer class and its fields.

---
+++ src/main/java/easy_eats/Customer.java
@@ -0,0 +1,8 @@
+package easy_eats;
+
+public class Customer {
+    private String customersName;
+    private String customersId;
+    private String customersAdress;
+    private String customersEmail;
+}
