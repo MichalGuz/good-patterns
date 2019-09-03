dd74df2cc9b135b849f5b4de1892bad6f47dfcb1
MichalGuz <michalguz@gmail.com>
2019-09-03 23:20:53 +0200

    Create classe of another producer with implementation of method process() from EasyEatsProcessor interface.

---
+++ src/main/java/easy_eats/OrganicFoodProducer.java
@@ -0,0 +1,8 @@
+package easy_eats;
+
+public class OrganicFoodProducer implements EasyEatsProcessor {
+    @Override
+    public void process(){
+        System.out.println("Organic food");
+    }
+}
