46c6f86a4eb91b5dd25e060008f560fa926554cb
MichalGuz <michalguz@gmail.com>
2019-09-05 01:18:57 +0200

    Create method main() in class EasyEatsMain. Create new objects of class Customer, Product, Order.

--- src/main/java/easy_eats/EasyEatsMain.java
+++ src/main/java/easy_eats/EasyEatsMain.java
@@ -1,4 +1,11 @@
 package easy_eats;
 
+import java.time.LocalDate;
+
 public class EasyEatsMain {
+    public static void main(String[] args) {
+        Customer customer1 = new Customer("John", "0001", "Poland", "john@john.com");
+        Product product1 = new Product(new OrganicFoodProducer(), "aa1", "P-ID 20433", 200.0);
+        Order order1 = new Order("190904-0001", LocalDate.of(2019, 9,4 ), 200.00);
+    }
 }
