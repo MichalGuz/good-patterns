8d0f1651c3cb6c4d83ff3e90000fee0d620e7c68
MichalGuz <michalguz@gmail.com>
2019-09-05 00:20:45 +0200

    Create class EasyEatsApplication and its fields

---
+++ src/main/java/easy_eats/EasyEatsApplication.java
@@ -0,0 +1,10 @@
+package easy_eats;
+
+public class EasyEatsApplication {
+    private EasyEatsOrderService service;
+    private EasyEatsOrderRepository repository;
+    private EasyEatsContactService contact;
+    private EasyEatsProducerProcessor producerProcessor;
+
+
+}
