aa46046514835c06b5d296bd392cb4b7ef80d4b6
MichalGuz <michalguz@gmail.com>
2019-09-05 00:09:35 +0200

    Amend the class Producer and transform it to an interface.

--- src/main/java/easy_eats/HealthyEatsProducer.java
+++ src/main/java/easy_eats/HealthyEatsProducer.java
@@ -1,6 +1,9 @@
 package easy_eats;
 
 public class HealthyEatsProducer implements EasyEatsProducerProcessor {
+    private String name;
+    private String adress
+
     @Override
     public void producersProcess(){
         System.out.println("HealthyEats");
--- src/main/java/easy_eats/Producer.java
+++ src/main/java/easy_eats/Producer.java
@@ -1,31 +1,35 @@
 package easy_eats;
 
-public class Producer {
-    private String name;
-    private String contact;
-    private String account;
-    private EasyEatsProducerProcessor processor;
+public interface Producer {
+    String getName();
+    String getContact();
+    String getAccount();
 
-    public Producer(String name, String contact, String account, EasyEatsProducerProcessor processor) {
-        this.name = name;
-        this.contact = contact;
-        this.account = account;
-        this.processor = processor;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public EasyEatsProducerProcessor getProcessor() {
-        return processor;
-    }
+//    private String name;
+//    private String contact;
+//    private String account;
+//    private EasyEatsProducerProcessor processor;
+//
+//    public Producer(String name, String contact, String account, EasyEatsProducerProcessor processor) {
+//        this.name = name;
+//        this.contact = contact;
+//        this.account = account;
+//        this.processor = processor;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getContact() {
+//        return contact;
+//    }
+//
+//    public String getAccount() {
+//        return account;
+//    }
+//
+//    public EasyEatsProducerProcessor getProcessor() {
+//        return processor;
+//    }
 }
