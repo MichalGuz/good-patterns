54ee3c06139000b11001037bd32773aca4bf3ac1
MichalGuz <michalguz@gmail.com>
2019-09-05 00:59:11 +0200

    Amend implementation of method order() of class EasyEatsService and implimentation of method createOrder() in class EasyEatsRepo.

--- src/main/java/easy_eats/EasyEatsService.java
+++ src/main/java/easy_eats/EasyEatsService.java
@@ -3,6 +3,12 @@ package easy_eats;
 public class EasyEatsService implements EasyEatsOrderService {
     @Override
     public boolean order(Customer customer, Product product, Order order) {
+        System.out.println("Order number: " + order.getOrderId() + "\n"
+                + "Date of placed of order: " + order.getDateOfOrder() + "\n"
+                + "Purchaser: " + customer.getCustomersName() + ", ID of User: " + customer.getCustomersId() + ".\n"
+                + "Details of the order:\n" + "[Product ID] " + product.getProductsId()
+                + "\n[Product] " + product.getProductsName() + "\n[Price] " + product.getProductsPrice()
+                + "\n[ID of order] " + order.getOrderId() + "\n[Total price: ] " + order.getTotalPrice());
         return true;
     }
 }
--- src/main/java/easy_eats/EasyEaysRepo.java
+++ src/main/java/easy_eats/EasyEaysRepo.java
@@ -3,12 +3,8 @@ package easy_eats;
 public class EasyEaysRepo implements EasyEatsOrderRepository {
     @Override
     public boolean createOrder(Customer customer, Product product, Order order) {
-        System.out.println("Order number: " + order.getOrderId() + "\n"
-                + "Date of placed of order: " + order.getDateOfOrder() + "\n"
-                + "Purchaser: " + customer.getCustomersName() + ", ID of User: " + customer.getCustomersId() + ".\n"
-                + "Details of the order:\n" + "[Product ID] " + product.getProductsId()
-                + "\n[Product] " + product.getProductsName() + "\n[Price] " + product.getProductsPrice()
-                + "\n[ID of order] " + order.getOrderId() + "\n[Total price: ] " + order.getTotalPrice());
+        System.out.println("Order number: " + order.getOrderId()
+                + " was saved in order repository");
         return true ;
     }
 }
