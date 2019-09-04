2d825fa6e662b2171af09cd08d0de76d49bedab8
MichalGuz <michalguz@gmail.com>
2019-09-05 00:34:08 +0200

    Implement method process() with 'if' condition.

--- src/main/java/easy_eats/EasyEatsApplication.java
+++ src/main/java/easy_eats/EasyEatsApplication.java
@@ -15,5 +15,13 @@ public class EasyEatsApplication {
 
     public void process(final OrderRequest orderRequest){
         boolean isOrder = service.order(orderRequest.getCustomer(), orderRequest.getProduct(), orderRequest.getOrder());
+
+        if(isOrder) {
+            repository.createOrder(orderRequest.getCustomer(), orderRequest.getProduct(), orderRequest.getOrder());
+            contact.inform(orderRequest.getCustomer());
+            producerProcessor.producersProcess();
+        } else {
+            System.out.println("Please check your order thoroughly.");
+        }
     }
 }
