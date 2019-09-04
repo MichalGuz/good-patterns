856b03de82f00b0070b3b43366d15941656093d6
MichalGuz <michalguz@gmail.com>
2019-09-05 01:20:21 +0200

    Create new objects of class OrderRequest and EasyEatsApplication in body of method main() in class EasyEatsMain.

--- src/main/java/easy_eats/EasyEatsMain.java
+++ src/main/java/easy_eats/EasyEatsMain.java
@@ -7,5 +7,10 @@ public class EasyEatsMain {
         Customer customer1 = new Customer("John", "0001", "Poland", "john@john.com");
         Product product1 = new Product(new OrganicFoodProducer(), "aa1", "P-ID 20433", 200.0);
         Order order1 = new Order("190904-0001", LocalDate.of(2019, 9,4 ), 200.00);
+
+        OrderRequest request = new OrderRequest(customer1, product1,order1);
+        EasyEatsApplication app = new EasyEatsApplication(new EasyEatsService(), new EasyEaysRepo(), new EasyEatsMail(), new OrganicFoodProducer());
+        app.process(request);
+
     }
 }
