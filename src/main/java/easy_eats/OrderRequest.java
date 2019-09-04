418d38c842019bcae464dd20c9bb9f01a006a571
MichalGuz <michalguz@gmail.com>
2019-09-04 23:38:43 +0200

    Create constructor and getters of class OrderRequest.

--- src/main/java/easy_eats/OrderRequest.java
+++ src/main/java/easy_eats/OrderRequest.java
@@ -5,5 +5,21 @@ public class OrderRequest {
     private Product product;
     private Order order;
 
+    public OrderRequest(Customer customer, Product product, Order order) {
+        this.customer = customer;
+        this.product = product;
+        this.order = order;
+    }
 
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
 }
