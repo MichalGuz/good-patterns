57f65d7cd8d180d1f949650e2292b058e5b339f2
MichalGuz <michalguz@gmail.com>
2019-09-05 00:10:23 +0200

    Complete implementation of class MasterFoodProducer.

--- src/main/java/easy_eats/MasterFoodProducer.java
+++ src/main/java/easy_eats/MasterFoodProducer.java
@@ -1,8 +1,23 @@
 package easy_eats;
 
-public class MasterFoodProducer implements EasyEatsProducerProcessor {
+public class MasterFoodProducer implements EasyEatsProducerProcessor, Producer {
    @Override
    public void producersProcess(){
        System.out.println("MasterFood");
    }
+
+    @Override
+    public String getName(){
+       return "MasterFoodProducer";
+    }
+
+    @Override
+    public String getContact(){
+       return "MFP@mail.com";
+    }
+
+    @Override
+    public String getAccount(){
+       return "account number of MFP";
+    }
 }
