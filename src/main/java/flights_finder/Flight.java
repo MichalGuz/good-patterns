9c1dc8b2369fee6397f9d07458fc54fff7eb229c
MichalGuz <michalguz@gmail.com>
2019-09-11 11:43:09 +0200

    Create constructor of Flight class and get() methods in this class.

--- src/main/java/flights_finder/Flight.java
+++ src/main/java/flights_finder/Flight.java
@@ -3,4 +3,17 @@ package flights_finder;
 public class Flight {
     private String departure;
     private String arrival;
+
+    public Flight(String departure, String arrival) {
+        this.departure = departure;
+        this.arrival = arrival;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
 }
