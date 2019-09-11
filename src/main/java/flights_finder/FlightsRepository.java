9490aed6bfbbd46eaaf2b045a5cb20db7f79e230
MichalGuz <michalguz@gmail.com>
2019-09-11 11:41:44 +0200

    Create FlightsRepository class and its field.

---
+++ src/main/java/flights_finder/FlightsRepository.java
@@ -0,0 +1,8 @@
+package flights_finder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightsRepository {
+    private List<Flight> listOfConnections = new ArrayList<>();
+}
