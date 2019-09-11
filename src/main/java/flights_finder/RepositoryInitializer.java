fa51a5eab124b7c4497431215738c14fd2a124dc
MichalGuz <michalguz@gmail.com>
2019-09-11 11:48:51 +0200

    Create fileds in  RepositoryInitializer class and addFlight() method.

--- src/main/java/flights_finder/RepositoryInitializer.java
+++ src/main/java/flights_finder/RepositoryInitializer.java
@@ -1,4 +1,10 @@
 package flights_finder;
 
 public class RepositoryInitializer {
+    FlightsRepository repository;
+    Flight flight;
+
+    void addFlight(Flight flight){
+        repository.getListOfConnections().add(flight);
+    }
 }
