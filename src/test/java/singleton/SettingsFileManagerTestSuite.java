304510f5c7593bcc737c944933eb43a2b5cabfaa
MichalGuz <michalguz@gmail.com>
2019-10-10 23:41:10 +0200

    Create openSettingsFIle().

--- src/test/java/singleton/SettingsFileManagerTestSuite.java
+++ src/test/java/singleton/SettingsFileManagerTestSuite.java
@@ -2,4 +2,9 @@ package singleton;
 
 public class SettingsFileManagerTestSuite {
     private static SettingsFileManager settingsFileManager;
+
+    public static void openSettingsFIle() {
+        settingsFileManager = new SettingsFileManager();
+        settingsFileManager.open("myapp.settings");
+    }
 }
