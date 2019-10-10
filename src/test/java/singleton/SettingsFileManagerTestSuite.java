b688e907648cd28c2dea4f7438d2fd224c5531d2
MichalGuz <michalguz@gmail.com>
2019-10-10 23:52:49 +0200

    Add @BeforeClass annotation to testGetFileName() method

--- src/test/java/singleton/SettingsFileManagerTestSuite.java
+++ src/test/java/singleton/SettingsFileManagerTestSuite.java
@@ -1,15 +1,19 @@
 package singleton;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SettingsFileManagerTestSuite {
     private static SettingsFileManager settingsFileManager;
 
+    @BeforeClass
     public static void openSettingsFIle() {
         settingsFileManager = new SettingsFileManager();
         settingsFileManager.open("myapp.settings");
     }
 
+    @AfterClass
     public static void closeSettingsFile() {
         settingsFileManager.close();
     }
