7bbdc214d6045a21bb46d16283ab4c943f1ca29c
MichalGuz <michalguz@gmail.com>
2019-10-10 23:46:57 +0200

    Create testGetFileName() method

--- src/test/java/singleton/SettingsFileManagerTestSuite.java
+++ src/test/java/singleton/SettingsFileManagerTestSuite.java
@@ -7,4 +7,12 @@ public class SettingsFileManagerTestSuite {
         settingsFileManager = new SettingsFileManager();
         settingsFileManager.open("myapp.settings");
     }
+
+    public static void closeSettingsFile() {
+        settingsFileManager.close();
+    }
+
+    public void testGetFileName() {
+
+    }
 }
