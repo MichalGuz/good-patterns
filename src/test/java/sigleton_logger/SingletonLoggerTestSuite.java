79eaeeebd84fc2bea61f2753110dab89b18115ba
MichalGuz <michalguz@gmail.com>
2019-10-13 18:41:25 +0200

    Add 'given' section of testGetLastLogger(): new object of Logger class and initilize String variable 'logger'."

--- src/test/java/sigleton_logger/SingletonLoggerTestSuite.java
+++ src/test/java/sigleton_logger/SingletonLoggerTestSuite.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 public class SingletonLoggerTestSuite {
     @Test
     public void testGetLastLogger(){
+        // given
+        Logger logger = new Logger();
+        String log = "2019.10.13 18:38 main_user";
 
-    }
+     }
 }
