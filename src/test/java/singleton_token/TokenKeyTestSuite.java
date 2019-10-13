d00e2700a45f91d248b2e91ea0a7b5d23d3c74c4
MichalGuz <michalguz@gmail.com>
2019-10-13 19:53:20 +0200

    Create 'then' section of testGetTokenKey(): check of working the getTokenKey() method.

--- src/test/java/singleton_token/TokenKeyTestSuite.java
+++ src/test/java/singleton_token/TokenKeyTestSuite.java
@@ -1,5 +1,6 @@
 package singleton_token;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TokenKeyTestSuite {
@@ -11,5 +12,8 @@ public class TokenKeyTestSuite {
         // when
         tokenKey.key(new String("78ad32feg45KW2"));
         String key = tokenKey.getTokenKey();
+
+        // then
+        Assert.assertEquals("78ad32feg45KW2", key);
     }
 }
