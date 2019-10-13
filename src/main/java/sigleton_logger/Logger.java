e88ce7b635844b237e6f6b66ac2ec522572450b5
MichalGuz <michalguz@gmail.com>
2019-10-13 18:50:50 +0200

    Add private constructor of class Logger without parameters.

--- src/main/java/sigleton_logger/Logger.java
+++ src/main/java/sigleton_logger/Logger.java
@@ -4,6 +4,8 @@ public class Logger {
     private static Logger logger = null;
     private String lastLog = "";
 
+    private Logger(){};
+
     public void log(String log){
         lastLog = log;
         System.out.println("Log [ " + log + "]");
