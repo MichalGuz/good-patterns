c5411d344a321688995037f3e24c828ac34d6b25
MichalGuz <michalguz@gmail.com>
2019-10-13 19:57:09 +0200

    Add private constructor of TokenKey class without parameters.

--- src/main/java/singleton_token/TokenKey.java
+++ src/main/java/singleton_token/TokenKey.java
@@ -4,6 +4,8 @@ public final class TokenKey {
     private static TokenKey tokenKeyInstance = null;
     private String tokenKey = "";
 
+    private TokenKey(){};
+
     public void key(String key){
         tokenKey = key;
         System.out.println("Key [" + key + "]");
