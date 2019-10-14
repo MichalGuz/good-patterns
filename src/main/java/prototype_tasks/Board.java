0e7f73ddeca1894b89be5ef31950dfbd13656d74
MichalGuz <michalguz@gmail.com>
2019-10-14 23:44:39 +0200

    Add two final fields  of class Board.

--- src/main/java/prototype_tasks/Board.java
+++ src/main/java/prototype_tasks/Board.java
@@ -1,4 +1,10 @@
 package prototype_tasks;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class Board {
-}
+    private final String name;
+    private final Set<TaskList> lists new HashSet;
+
+
