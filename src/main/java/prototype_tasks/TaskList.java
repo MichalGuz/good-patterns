2522fa03205443b21ee440f0e4078d231a395685
MichalGuz <michalguz@gmail.com>
2019-10-14 23:30:43 +0200

    Create two fields of class TaskList.

--- src/main/java/prototype_tasks/TaskList.java
+++ src/main/java/prototype_tasks/TaskList.java
@@ -1,4 +1,9 @@
 package prototype_tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class TaskList {
+    final private String name;
+    final private List<Task> tasks = new ArrayList<>();
 }
