ba3872a15efff1e3f64ba3167593f1dff1fb50c1
MichalGuz <michalguz@gmail.com>
2019-10-14 23:27:07 +0200

    Override method toString() in class Task.

--- src/main/java/prototype_tasks/Task.java
+++ src/main/java/prototype_tasks/Task.java
@@ -10,4 +10,9 @@ public final class Task {
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return "         Task [" + name + "]";
+    }
 }
