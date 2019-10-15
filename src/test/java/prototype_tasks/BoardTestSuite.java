c43290de39e5823acbe1995047d6fd40d83a4169
MichalGuz <michalguz@gmail.com>
2019-10-15 23:56:33 +0200

    Create 'given' section of  method testToString(): create new object TaskList listToDo.

--- src/test/java/prototype_tasks/BoardTestSuite.java
+++ src/test/java/prototype_tasks/BoardTestSuite.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 public class BoardTestSuite {
     @Test
     public void testToString(){
+        // given
+        // creating the TaskList for "to do"
+        TaskList listToDo = new TaskList("To Do Tasks");
 
     }
+
+
 }
