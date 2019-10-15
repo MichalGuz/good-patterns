444ee4747e56487957c50e66d20df3cf2b60adb5
MichalGuz <michalguz@gmail.com>
2019-10-15 23:59:28 +0200

    Add in 'given' section of  method testToString() IntStream to fill the TaskList with Task objects.

--- src/test/java/prototype_tasks/BoardTestSuite.java
+++ src/test/java/prototype_tasks/BoardTestSuite.java
@@ -2,12 +2,17 @@ package prototype_tasks;
 
 import org.junit.Test;
 
+import java.util.stream.IntStream;
+
 public class BoardTestSuite {
     @Test
     public void testToString(){
         // given
         // creating the TaskList for "to do"
         TaskList listToDo = new TaskList("To Do Tasks");
+        IntStream.iterate(1, n -> n +1)
+                .limit(10)
+                .forEach(n -> listToDo.getTasks().add(new Task("To Do Task number" + n)));
 
     }
 
