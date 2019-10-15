24d33daa4a630f04526a5fcc5edd1cc02fa9843c
MichalGuz <michalguz@gmail.com>
2019-10-16 00:36:09 +0200

    Add in 'given' section of the method testToString() the method println() of Board object.

--- src/test/java/prototype_tasks/BoardTestSuite.java
+++ src/test/java/prototype_tasks/BoardTestSuite.java
@@ -6,29 +6,31 @@ import java.util.stream.IntStream;
 
 public class BoardTestSuite {
     @Test
-    public void testToString(){
+    public void testToString() {
         // given
         // creating the TaskList for "to do"
         TaskList listToDo = new TaskList("To Do Tasks");
-        IntStream.iterate(1, n -> n +1)
+        IntStream.iterate(1, n -> n + 1)
                 .limit(10)
                 .forEach(n -> listToDo.getTasks().add(new Task("To Do Task number" + n)));
 
-    // creating the TaskList for tasks in progress
+        // creating the TaskList for tasks in progress
         TaskList listInProgress = new TaskList("In Progress Tasks");
         IntStream.iterate(1, n -> n + 1)
                 .limit(10)
                 .forEach(n -> listInProgress.getTasks().add(new Task("In Progress Task" + n)));
 
-    // creating the TaskList for done tasks
+        // creating the TaskList for done tasks
         TaskList listDone = new TaskList("Done Tasks");
-        IntStream.iterate(1, n -> n +1)
+        IntStream.iterate(1, n -> n + 1)
                 .limit(10)
                 .forEach(n -> listDone.getTasks().add(new Task("Done Task" + n)));
 
         // creating the Board and assising  the lists
         Board board = new Board("Project #1");
         board.getLists().add(listToDo);
-        board.getLists().add(listInProgress)
+        board.getLists().add(listInProgress);
         board.getLists().add(listDone);
+        System.out.println(board);
     }
+}
