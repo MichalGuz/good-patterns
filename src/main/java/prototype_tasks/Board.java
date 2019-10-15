cbd08b7cc196ecbf4c3259338ddc2802156293e7
MichalGuz <michalguz@gmail.com>
2019-10-15 23:43:57 +0200

    Amend initailization of variable 'lists' in Board class.

--- src/main/java/prototype_tasks/Board.java
+++ src/main/java/prototype_tasks/Board.java
@@ -1,10 +1,11 @@
 package prototype_tasks;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public final class Board {
     private final String name;
-    private final Set<TaskList> lists new hashSet();
+    private final Set<TaskList> lists = new HashSet<>();
 
     public Board(String name) {
         this.name = name;
