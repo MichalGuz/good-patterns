426144eec759b08689dc5407a569fd4cd1da40f0
MichalGuz <michalguz@gmail.com>
2019-10-16 19:30:09 +0200

    Override T clone() method of class Object in class Prototype.

--- src/main/java/prototype_tasks/Prototype.java
+++ src/main/java/prototype_tasks/Prototype.java
@@ -1,4 +1,8 @@
 package prototype_tasks;
 
-public class Prototype {
+public class Prototype <T> implements Cloneable{
+    @Override
+    public T clone(){
+        return (T)super.clone();
+    }
 }
