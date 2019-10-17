7dd3f2f3cf93f07a2c5e006e0646aadb006f24bc
MichalGuz <michalguz@gmail.com>
2019-10-17 21:23:51 +0200

    Create constructor of class Book.

--- src/main/java/prototype_library/Book.java
+++ src/main/java/prototype_library/Book.java
@@ -6,4 +6,10 @@ public final class Book {
     final String title;
     final String author;
     final LocalDate dateOfRelease;
+
+    public Book(final String title, final String author, final LocalDate dateOfRelease) {
+        this.title = title;
+        this.author = author;
+        this.dateOfRelease = dateOfRelease;
+    }
 }
