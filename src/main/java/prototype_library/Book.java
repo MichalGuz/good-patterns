ad91264efd2e4ec8fba6ac6b36eb1aaba4804176
MichalGuz <michalguz@gmail.com>
2019-10-17 21:22:57 +0200

    Add keyword final to class Book and create three final fields of this class .

--- src/main/java/prototype_library/Book.java
+++ src/main/java/prototype_library/Book.java
@@ -1,4 +1,9 @@
 package prototype_library;
 
-public class Book {
+import java.time.LocalDate;
+
+public final class Book {
+    final String title;
+    final String author;
+    final LocalDate dateOfRelease;
 }
