89f48d70c36c5117ccfe8ce366c2eb9211396e95
MichalGuz <michalguz@gmail.com>
2019-10-18 00:13:42 +0200

    Add for loop to create Book objects and fill the set of books in library - in 'given' sectionf testGetBook().

--- src/test/java/prototype_library/LibraryTestSuite.java
+++ src/test/java/prototype_library/LibraryTestSuite.java
@@ -2,11 +2,18 @@ package prototype_library;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class LibraryTestSuite {
     @Test
     public void testGetBooks() {
         // given
         Library library = new Library("library #1");
-
-       }
+        for (int i = 0; i < 20; i++) {
+            String title = "title" + i;
+            String author = "author" + i;
+            LocalDate localDate = new RandomDate().getRandomDate();
+            library.getBooks().add(new Book(title, author, localDate));
+        }
+     }
 }
