88aa3dc93e13cfac6f10ffe373c4add404f70c18
MichalGuz <michalguz@gmail.com>
2019-10-17 21:49:07 +0200

    Create two final fields of class Library.

--- src/main/java/prototype_library/Library.java
+++ src/main/java/prototype_library/Library.java
@@ -1,4 +1,9 @@
 package prototype_library;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class Library {
+    final String name;
+    final Set<Book> books = new HashSet<>();
 }
