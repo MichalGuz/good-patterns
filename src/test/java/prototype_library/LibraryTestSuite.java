71381a3c2fbf44af8ba89d5ffe56eaa13758d169
MichalGuz <michalguz@gmail.com>
2019-10-17 23:10:56 +0200

    Create class RandomDate.

--- src/test/java/prototype_library/LibraryTestSuite.java
+++ src/test/java/prototype_library/LibraryTestSuite.java
@@ -1,7 +1,12 @@
 package prototype_library;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class LibraryTestSuite {
     public void testGetBooks() {
+        // given
+        Random generator = new Random();
 
     }
 }
