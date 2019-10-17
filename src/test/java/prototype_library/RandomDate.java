008a6d00d64eb441302cff39634df711349ab639
MichalGuz <michalguz@gmail.com>
2019-10-17 23:12:44 +0200

    Create new object of Random class and two int fields initialized by invocation of nextInt() method in class RandomDate.

--- src/test/java/prototype_library/RandomDate.java
+++ src/test/java/prototype_library/RandomDate.java
@@ -1,4 +1,10 @@
 package prototype_library;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class RandomDate {
+    private Random generator = new Random();
+    private int randomDayOfYear= generator.nextInt(365);
+    private int randomYear = (generator.nextInt(119) + 1900);
 }
