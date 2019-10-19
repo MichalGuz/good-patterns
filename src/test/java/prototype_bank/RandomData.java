0e14469d5ee9595f010393f311fb79af75b4917d
MichalGuz <michalguz@gmail.com>
2019-10-19 23:14:51 +0200

    Create fields of class RandomData and methods getRandomDate() and getRandomAmount().

--- src/test/java/prototype_bank/RandomData.java
+++ src/test/java/prototype_bank/RandomData.java
@@ -1,4 +1,21 @@
 package prototype_bank;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class RandomData {
+    private Random generator = new Random();
+    private double randomAmount = (generator.nextDouble());
+    private int randomDayOfYear = (generator.nextInt(364) + 1);
+    private int randomYear = (generator.nextInt(2) + 2017);
+
+    public LocalDate getRandomDate() {
+        LocalDate randomDate = LocalDate.ofYearDay(randomYear, randomDayOfYear);
+        return randomDate;
+    }
+
+    public double getRandomAmount(){
+        return randomAmount * 100;
+    }
+
 }
