b5ab02e678a9848326f5863cf27b57223f69918e
MichalGuz <michalguz@gmail.com>
2019-10-19 20:45:00 +0200

    Create final fields of Transaction class.

--- src/main/java/prototype_bank/Transaction.java
+++ src/main/java/prototype_bank/Transaction.java
@@ -1,4 +1,12 @@
 package prototype_bank;
 
+import java.time.LocalDate;
+
 public final class Transaction {
+    private final String transactionId;
+    private final int transcationAmount;
+    private final LocalDate transactionDate;
+
+
+
 }
