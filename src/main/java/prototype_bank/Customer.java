ace6f0b7442b26c5b462910500b1425132c1071a
MichalGuz <michalguz@gmail.com>
2019-10-19 21:52:46 +0200

    Create final fields of Customer class.

--- src/main/java/prototype_bank/Customer.java
+++ src/main/java/prototype_bank/Customer.java
@@ -1,4 +1,10 @@
 package prototype_bank;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class Customer {
+    private final String customerId;
+    private final Set<Account>() = new HashSet<>();
+
 }
