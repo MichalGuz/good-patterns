08c24d300bc1ec45ce84af1bddacfd46d2036dd3
MichalGuz <michalguz@gmail.com>
2019-10-19 22:11:49 +0200

    Create final fields of Bank class.

--- src/main/java/prototype_bank/Bank.java
+++ src/main/java/prototype_bank/Bank.java
@@ -1,4 +1,10 @@
 package prototype_bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Bank {
+    private final String bankBranch;
+    private final List<Customer> customers = new ArrayList<>();
+
 }
