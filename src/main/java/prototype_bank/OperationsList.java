c46f1e478fc0088355242b3e0019a36edfa8fe61
MichalGuz <michalguz@gmail.com>
2019-10-19 21:22:44 +0200

    Create final fields of OperationLists class.

--- src/main/java/prototype_bank/OperationsList.java
+++ src/main/java/prototype_bank/OperationsList.java
@@ -1,4 +1,9 @@
 package prototype_bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class OperationsList {
+    private final String name;
+    private final List<Transaction> transactions = new ArrayList<>(;)
 }
