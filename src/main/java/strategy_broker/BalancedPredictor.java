1549143cf5f90407ba011f03a1b621f6a0a1610b
MichalGuz <michalguz@gmail.com>
2019-10-21 19:17:25 +0200

    Override predictWhatToBuy() method inside class BalancedPredictor.

--- src/main/java/strategy_broker/BalancedPredictor.java
+++ src/main/java/strategy_broker/BalancedPredictor.java
@@ -1,4 +1,9 @@
 package strategy_broker;
 
 public class BalancedPredictor implements BuyPredictor{
+    @Override
+    public String predictWhatToBuy() {
+        return "[Balanced predictor] buy shared units of Famous-&-Old Fund";
+
+    }
 }
