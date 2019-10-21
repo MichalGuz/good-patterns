cdbe303d5fc38c136e17ac5f9051c80911b325c0
MichalGuz <michalguz@gmail.com>
2019-10-21 21:58:46 +0200

    Create 'when' section of testDefaultInvestingStrategy(): add String variable and invoke method predictWhatToBuy() using interface Buy Predictor 'buyPredictor' and Customer 'bold' objects.

--- src/test/java/strategy_broker/CustomerTestSuite.java
+++ src/test/java/strategy_broker/CustomerTestSuite.java
@@ -9,6 +9,10 @@ public class CustomerTestSuite {
         Customer bold = new IndividualYoungCustomer("Adam Young");
         Customer cautious = new IndividualOldCustomer("Ian Older");
         Customer corporate = new CorporateCustomer("Unity Ltd");
+
+        // when
+        String boldShouldBuy = bold.buyPredictor.predictWhatToBuy();
+        System.out.println(bold.getName() + " should buy: " +  boldShouldBuy);
     }
 }
 
