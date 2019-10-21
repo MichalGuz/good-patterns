7255048fd1ec385b71a363976cb07560b4b1ecd3
MichalGuz <michalguz@gmail.com>
2019-10-21 19:43:42 +0200

    Amend the constructor of class CorporateCustomer to choose specific BuyPredictor according to appropriate strategy.

--- src/main/java/strategy_broker/CorporateCustomer.java
+++ src/main/java/strategy_broker/CorporateCustomer.java
@@ -3,5 +3,6 @@ package strategy_broker;
 public class CorporateCustomer extends Customer {
     public CorporateCustomer(String name) {
         super(name);
+        this.buyPredictor = new BalancedPredictor();
     }
 }
