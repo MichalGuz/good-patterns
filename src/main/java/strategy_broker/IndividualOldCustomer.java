65e970f2f0df1961735e9d087ccb1e646661c61f
MichalGuz <michalguz@gmail.com>
2019-10-21 19:45:44 +0200

    Amend the constructor of class IndividualOldCustomer to choose specific BuyPredictor according to appropriate strategy.

--- src/main/java/strategy_broker/IndividualOldCustomer.java
+++ src/main/java/strategy_broker/IndividualOldCustomer.java
@@ -3,5 +3,6 @@ package strategy_broker;
 public class IndividualOldCustomer extends Customer {
     public IndividualOldCustomer(String name) {
         super(name);
+        this.buyPredictor = new ConservativePredictor();
     }
 }
