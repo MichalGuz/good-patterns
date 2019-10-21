543008142cab63656bb0627aacd55ce51eba7198
MichalGuz <michalguz@gmail.com>
2019-10-21 19:24:50 +0200

    Override predictWhatToBuy() method inside class ConservativePredictor.

--- src/main/java/strategy_broker/ConservativePredictor.java
+++ src/main/java/strategy_broker/ConservativePredictor.java
@@ -1,5 +1,8 @@
 package strategy_broker;
 
 public class ConservativePredictor implements BuyPredictor {
-
+    @Override
+    public String predictWhatToBuy() {
+        return "[Conservative predictor] buy safe public bonds";
+    }
 }
