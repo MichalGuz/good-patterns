0ce64f8577e46b38188d2a64af791d138b64338b
MichalGuz <michalguz@gmail.com>
2019-10-21 19:18:43 +0200

    Amend the code: remove useless space and add needed space

--- src/main/java/strategy_broker/BalancedPredictor.java
+++ src/main/java/strategy_broker/BalancedPredictor.java
@@ -1,9 +1,8 @@
 package strategy_broker;
 
-public class BalancedPredictor implements BuyPredictor{
+public class BalancedPredictor implements BuyPredictor {
     @Override
     public String predictWhatToBuy() {
         return "[Balanced predictor] buy shared units of Famous-&-Old Fund";
-
     }
 }
