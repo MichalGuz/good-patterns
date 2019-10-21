43e77ee3ca2527f7c3cac200b97dcd88f5579206
MichalGuz <michalguz@gmail.com>
2019-10-21 19:13:05 +0200

    Override predictWhatToBuy() method inside class AggressivePredictorr.

--- src/main/java/strategy_broker/AggressivePredictor.java
+++ src/main/java/strategy_broker/AggressivePredictor.java
@@ -1,5 +1,8 @@
 package strategy_broker;
 
 public class AggressivePredictor implements BuyPredictor {
-
+    @Override
+    public String predictWhatToBuy() {
+        return "[Aggressive predictor] buy stock of MysteriousName Inc";
+    }
 }
