450d88ea1e79de98df3897888ea8aab0d0678ae8
MichalGuz <michalguz@gmail.com>
2019-10-21 21:49:49 +0200

    Create 'given' section of testDefaultInvestingStrategy() with three new objects of classes implementing Customer interface.

--- src/test/java/strategy_broker/CustomerTestSuite.java
+++ src/test/java/strategy_broker/CustomerTestSuite.java
@@ -4,7 +4,12 @@ import org.junit.Test;
 
 public class CustomerTestSuite {
     @Test
-    public void testDefaultInvestingStrategy() {}
+    public void testDefaultInvestingStrategy() {
+        // given
+        Customer bold = new IndividualYoungCustomer("Adam Young");
+        Customer cautious = new IndividualOldCustomer("Ian Older");
+        Customer corporate = new CorporateCustomer("Unity Ltd");
+    }
 }
 
 
