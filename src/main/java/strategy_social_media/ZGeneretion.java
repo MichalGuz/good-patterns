511180adec278c3d7ddf961e4c3eca69c664144a
MichalGuz <michalguz@gmail.com>
2019-10-22 19:50:16 +0200

    Add (in the consructor of ZGeneretion class) this operator for socialPublisher craeting new SnapchatPublisher() object.

--- src/main/java/strategy_social_media/ZGeneretion.java
+++ src/main/java/strategy_social_media/ZGeneretion.java
@@ -3,5 +3,6 @@ package strategy_social_media;
 public class ZGeneretion extends User {
     public ZGeneretion(String name) {
         super(name);
+        this.socialPublisher = new SnapchatPublisher();
     }
 }
