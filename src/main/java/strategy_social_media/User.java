bda34c709174c07898e84d681f6550026a62b515
MichalGuz <michalguz@gmail.com>
2019-10-22 19:40:31 +0200

    Add protected field SocialPublisher socialPublisher in the User class.

--- src/main/java/strategy_social_media/User.java
+++ src/main/java/strategy_social_media/User.java
@@ -2,6 +2,7 @@ package strategy_social_media;
 
 public class User {
     private final String name;
+    protected SocialPublisher socialPublisher;
 
     public User(String name) {
         this.name = name;
