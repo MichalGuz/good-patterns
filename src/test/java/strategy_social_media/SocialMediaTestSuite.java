4a98e476c2ae905b9b503db2009e7c921bc0e29b
MichalGuz <michalguz@gmail.com>
2019-10-22 20:17:17 +0200

    Create 'given' section of testDefaultSocialMediaMessagesSending(): create new objects of class extending User class.

--- src/test/java/strategy_social_media/SocialMediaTestSuite.java
+++ src/test/java/strategy_social_media/SocialMediaTestSuite.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 public class SocialMediaTestSuite {
     @Test
     public void testDefaultSocialMediaMessagesSending() {
-
+        // given
+        Millenials millenials = new Millenials("Neo");
+        YGeneretion yMan = new YGeneretion("Justin");
+        ZGeneretion zMan = new ZGeneretion("Stormzy");
     }
 }
