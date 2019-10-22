fee1ba2343daad776f709ee31f17b287f8acdc70
MichalGuz <michalguz@gmail.com>
2019-10-22 19:49:13 +0200

    Add (in the consructor of YGeneretion class) this operator for socialPublisher craeting new TwitterPublisher() object.

--- src/main/java/strategy_social_media/YGeneretion.java
+++ src/main/java/strategy_social_media/YGeneretion.java
@@ -3,5 +3,6 @@ package strategy_social_media;
 public class YGeneretion extends User {
     public YGeneretion(String name) {
         super(name);
+        this.socialPublisher = new TwitterPublisher();
     }
 }
