25f236175ebccc9c2ceea1d3e536526459261af6
MichalGuz <michalguz@gmail.com>
2019-10-22 19:10:10 +0200

    Override  method share() in the class TwitterPublisher.

--- src/main/java/strategy_social_media/TwitterPublisher.java
+++ src/main/java/strategy_social_media/TwitterPublisher.java
@@ -1,4 +1,8 @@
 package strategy_social_media;
 
 public class TwitterPublisher implements SocialPublisher {
+    @Override
+    public void share() {
+        System.out.println("Twitter");
+    }
 }
