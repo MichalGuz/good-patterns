576e2b6ce60fb60c8d7bc4bd32fbf52f401af923
MichalGuz <michalguz@gmail.com>
2019-10-22 19:33:12 +0200

    Override  method share() in the class SnapchatPublisher.

--- src/main/java/strategy_social_media/SnapchatPublisher.java
+++ src/main/java/strategy_social_media/SnapchatPublisher.java
@@ -1,5 +1,8 @@
 package strategy_social_media;
 
 public class SnapchatPublisher implements SocialPublisher {
-
+    @Override
+    public void share() {
+        System.out.println("Snapchat");
+    }
 }
