42d2103a6e077c9935c7ac2d5dbd304d1379e16f
MichalGuz <michalguz@gmail.com>
2019-10-22 19:08:43 +0200

    Override  method share() in the class FacebookPublisher.

--- src/main/java/strategy_social_media/FacebookPublisher.java
+++ src/main/java/strategy_social_media/FacebookPublisher.java
@@ -1,4 +1,8 @@
 package strategy_social_media;
 
 public class FacebookPublisher implements SocialPublisher {
+    @Override
+    public void share() {
+        System.out.println("Facebook");
+    }
 }
