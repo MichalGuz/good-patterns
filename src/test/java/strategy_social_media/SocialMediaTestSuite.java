fde0807b72b2e4bdadfbdf92633f837cbb80148a
MichalGuz <michalguz@gmail.com>
2019-10-22 22:17:20 +0200

    Amend the code inside 'when' section of testIndividualPreferencesAboutSocialMedia(): add new String variable for secondChoiceOfNeo.

--- src/test/java/strategy_social_media/SocialMediaTestSuite.java
+++ src/test/java/strategy_social_media/SocialMediaTestSuite.java
@@ -34,6 +34,7 @@ public class SocialMediaTestSuite {
         String defaultChoiceOfNeo = millenials.sendMessage();
         System.out.println(millenials.getName() + "'s first choice is " + defaultChoiceOfNeo + ".");
         millenials.setSocialPublisher(new TwitterPublisher());
-        System.out.println("But " + millenials.getName() + " also likes to use " + millenials.sendMessage() + ".");
+        String secondChoiceOfNeo = millenials.sendMessage();
+        System.out.println("But " + millenials.getName() + " also likes to use " + secondChoiceOfNeo + ".");
     }
 }
