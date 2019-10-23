76d6dd2f6c8726c07bc91e1b015fd1a4545068c7
MichalGuz <michalguz@gmail.com>
2019-10-23 23:30:09 +0200

    Create final fields 'name' and 'width' in class Square. Create constructor of class Square.

--- src/main/java/factory_shape/Square.java
+++ src/main/java/factory_shape/Square.java
@@ -1,5 +1,11 @@
 package factory_shape;
 
 public final class Square implements Shape {
+    final String name;
+    final double width;
 
+    public Square(String name, double width) {
+        this.name = name;
+        this.width = width;
+    }
 }
