5f246320f89de579430783aaa4bca74761595de3
MichalGuz <michalguz@gmail.com>
2019-10-24 00:10:55 +0200

    Create final static String for diffrente shapes in class ShapeFactory.

--- src/main/java/factory_shape/ShapeFactory.java
+++ src/main/java/factory_shape/ShapeFactory.java
@@ -1,4 +1,9 @@
 package factory_shape;
 
 public final class ShapeFactory {
+    public static final String CIRCLE = "CIRCLE";
+    public static final String SQUARE = "SQUARE";
+    public static final String RECTANGLE = "RECTANGLE";
+
+
 }
