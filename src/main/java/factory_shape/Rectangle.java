7c88f175809e406a292bfa66a679ce81a68cc0ec
MichalGuz <michalguz@gmail.com>
2019-10-23 23:48:35 +0200

    Create final fields 'name' width and 'lenght' in class Rectangle. Create constructor of class Rectangle.

--- src/main/java/factory_shape/Rectangle.java
+++ src/main/java/factory_shape/Rectangle.java
@@ -1,4 +1,13 @@
 package factory_shape;
 
 public final class Rectangle implements Shape {
+    final String name;
+    final double width;
+    final double lenght;
+
+    public Rectangle(String name, double width, double lenght) {
+        this.name = name;
+        this.width = width;
+        this.lenght = lenght;
+    }
 }
