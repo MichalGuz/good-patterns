9891b20e84f940f82afc9f5a6ec21684ef1bd840
MichalGuz <michalguz@gmail.com>
2019-10-24 00:13:38 +0200

    Create methood of Shape 'makeShape' in class ShapeFactory.

--- src/main/java/factory_shape/ShapeFactory.java
+++ src/main/java/factory_shape/ShapeFactory.java
@@ -5,5 +5,7 @@ public final class ShapeFactory {
     public static final String SQUARE = "SQUARE";
     public static final String RECTANGLE = "RECTANGLE";
 
+    public final Shape makeShapoe(final String shapeClass) {
 
+    }
 }
