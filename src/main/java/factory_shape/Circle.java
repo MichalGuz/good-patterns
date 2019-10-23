2738381b367546d573fd88f65d6ad21d11f4af1f
MichalGuz <michalguz@gmail.com>
2019-10-23 23:09:24 +0200

    Create final fields 'name' and 'radius' in class Circle. Create constructor of class Cirlce.

--- src/main/java/factory_shape/Circle.java
+++ src/main/java/factory_shape/Circle.java
@@ -1,5 +1,11 @@
 package factory_shape;
 
 public final class Circle implements Shape {
+    final String name;
+    final double radius;
 
+    public Circle(String name, double radius) {
+        this.name = name;
+        this.radius = radius;
+    }
 }
