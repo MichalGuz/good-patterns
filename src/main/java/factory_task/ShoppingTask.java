27e58712948857a468d98aefed59176f7c95fbd3
MichalGuz <michalguz@gmail.com>
2019-10-24 21:23:35 +0200

    Create constructor of class ShoppingTask.

--- src/main/java/factory_task/ShoppingTask.java
+++ src/main/java/factory_task/ShoppingTask.java
@@ -4,4 +4,10 @@ public class ShoppingTask {
     private String taskName;
     private String whatToBuy;
     private double quantity;
+
+    public ShoppingTask(String taskName, String whatToBuy, double quantity) {
+        this.taskName = taskName;
+        this.whatToBuy = whatToBuy;
+        this.quantity = quantity;
+    }
 }
