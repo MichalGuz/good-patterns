4b1619f83db07d54d7640e48dd6686ac4c8803c5
MichalGuz <michalguz@gmail.com>
2019-10-25 00:02:10 +0200

    Inside the body of method processTask() add two cases STORING_TASK and PAINTING_TASK.

--- src/main/java/factory_task/TaskFactory.java
+++ src/main/java/factory_task/TaskFactory.java
@@ -9,7 +9,12 @@ public class TaskFactory {
         switch (taskClass) {
             case SHOPPING_TASK:
                 return new ShoppingTask("Easter task #1", "eggs", 10);
-
+            case PAINTING_TASK:
+                return new PaintingTask("Easter task #2", "red", "boiled eggs");
+            case STORING_TASK:
+                return new StoringTask("Easter task #3", "coloured eggs", "fridge");
+                default:
+                    return null;
         }
     }
 
