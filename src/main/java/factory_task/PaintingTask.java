b088bc0763a19087399794560c3f24b9779a1f15
MichalGuz <michalguz@gmail.com>
2019-10-24 21:50:50 +0200

    Create constructor of class PaintingTask.

--- src/main/java/factory_task/PaintingTask.java
+++ src/main/java/factory_task/PaintingTask.java
@@ -4,4 +4,10 @@ public class PaintingTask {
     private String taskName;
     private String color;
     private String whatToColor;
+
+    public PaintingTask(String taskName, String color, String whatToColor) {
+        this.taskName = taskName;
+        this.color = color;
+        this.whatToColor = whatToColor;
+    }
 }
