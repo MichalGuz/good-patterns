5c3ddfa2411fc5ef074003cc2649ebc8b6baa18a
MichalGuz <michalguz@gmail.com>
2019-10-24 23:41:38 +0200

    Create static variables in TaskFactory class representing three classes which implementing interface Task.

--- src/main/java/factory_task/TaskFactory.java
+++ src/main/java/factory_task/TaskFactory.java
@@ -1,4 +1,9 @@
 package factory_task;
 
 public class TaskFactory {
+    private static final String SHOPPING_TASK = "SHOPPING";
+    private static final String PAINTING_TASK = "PAITING";
+    private static final String STORING_TASK = "STORING";
+
+
 }
