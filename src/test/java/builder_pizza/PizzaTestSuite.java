6b794a7f3269d3d1c0fee78f97c283e20dfc999c
MichalGuz <michalguz@gmail.com>
2019-10-26 16:05:45 +0200

    Invoek method println() in 'given' section of testPizzaNew() to display pizza in the console.

--- src/test/java/builder_pizza/PizzaTestSuite.java
+++ src/test/java/builder_pizza/PizzaTestSuite.java
@@ -7,5 +7,6 @@ public class PizzaTestSuite {
     public void testPizzaNew() {
         // given
         Pizza pizza = new Pizza("Thin", "Mild", "Mushrooms", "Pepper", "Onion", "Garlic");
+        System.out.println(pizza);
     }
 }
