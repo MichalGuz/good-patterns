897700cf755378062f2974608bb24b830aeb20fc
MichalGuz <michalguz@gmail.com>
2019-10-26 15:48:37 +0200

    Create final fields of Pizza class.

--- src/main/java/builder_pizza/Pizza.java
+++ src/main/java/builder_pizza/Pizza.java
@@ -1,4 +1,11 @@
 package builder_pizza;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Pizza {
+    private final String bottom;
+    private final String sauce;
+    private final List<String> ingredients = new ArrayList<>();
+
 }
