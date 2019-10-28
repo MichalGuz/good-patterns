a7c0fbcf06410e2c5f32456a196294b3cbfb4733
MichalGuz <michalguz@gmail.com>
2019-10-28 20:34:21 +0100

    Create Figure makeFigure() method inside FigureFactory class.

--- src/main/java/builder_checkers/FugureFactory.java
+++ src/main/java/builder_checkers/FugureFactory.java
@@ -3,4 +3,6 @@ package builder_checkers;
 public final class FugureFactory {
     public static final String PAWN = "PAWN";
     public static final String QUEEN = "QUEEN";
+
+    public static Figure makeFigure(String figureTyoe, String color) {}
 }
