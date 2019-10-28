8e9ae7a867c5e08d88e1e41bcde6f9cefe865636
MichalGuz <michalguz@gmail.com>
2019-10-28 20:41:10 +0100

    Inside body of the makeFigure() method add 'if' condition returned new Pawn object with 'color' argument.

--- src/main/java/builder_checkers/FugureFactory.java
+++ src/main/java/builder_checkers/FugureFactory.java
@@ -4,5 +4,9 @@ public final class FugureFactory {
     public static final String PAWN = "PAWN";
     public static final String QUEEN = "QUEEN";
 
-    public static Figure makeFigure(String figureTyoe, String color) {}
+    public static Figure makeFigure(String figureTyoe, String color) {
+        if (figureTyoe.equals(PAWN)) {
+            return new Pawn(color);
+        }
+    }
 }
