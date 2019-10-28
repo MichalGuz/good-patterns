caedb46cdf35aed452c428adde5597ee6f127fec
MichalGuz <michalguz@gmail.com>
2019-10-28 20:04:49 +0100

    Create String field 'color' in class Figure.

--- src/main/java/builder_checkers/Figure.java
+++ src/main/java/builder_checkers/Figure.java
@@ -3,4 +3,7 @@ package builder_checkers;
 public class Figure {
     public static final String WHITE = "WHITE";
     public static final String BLACK= "BLACK";
+    private final String color;
+
+
 }
