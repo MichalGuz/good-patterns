3fce4bbb7b67ab05a9fc1423ef8a0bb50c27d148
MichalGuz <michalguz@gmail.com>
2019-10-29 17:58:47 +0100

    Create constructor  of class Checkers.

--- src/main/java/builder_checkers/Checkers.java
+++ src/main/java/builder_checkers/Checkers.java
@@ -4,4 +4,10 @@ public class Checkers {
     private final Board board;
     private final String playerOne;
     private final String playerTwo;
+
+    public Checkers(Board board, String playerOne, String playerTwo) {
+        this.board = board;
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
 }
