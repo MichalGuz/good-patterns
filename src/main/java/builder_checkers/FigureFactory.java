90bd9ad371baa0ecceee15b23f5d1407382cf3dc
MichalGuz <michalguz@gmail.com>
2019-10-31 00:22:49 +0100

    Correct the typo in name 'figureType'.

--- src/main/java/builder_checkers/FigureFactory.java
+++ src/main/java/builder_checkers/FigureFactory.java
@@ -4,10 +4,10 @@ public final class FigureFactory {
     public static final String PAWN = "PAWN";
     public static final String QUEEN = "QUEEN";
 
-    public static Figure makeFigure(String figureTyoe, String color) {
-        if (figureTyoe.equals(PAWN)) {
+    public static Figure makeFigure(String figureType, String color) {
+        if (figureType.equals(PAWN)) {
             return new Pawn(color);
-        } else if (figureTyoe.equals(QUEEN)) {
+        } else if (figureType.equals(QUEEN)) {
             return new Queen(color);
         } else {
             throw new IllegalStateException("Type of figure should be pawn or queen.");
