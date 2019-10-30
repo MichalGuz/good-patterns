fa3dde3de6abfe75d6212a7d321b112fb268a076
MichalGuz <michalguz@gmail.com>
2019-10-30 14:49:21 +0100

    Create 'given' section of testCheckersBuilder(): create new Checkers object using constructor of class CheckersBuildder.

--- src/test/java/builder_checkers/CheckersTestSuite.java
+++ src/test/java/builder_checkers/CheckersTestSuite.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 public class CheckersTestSuite {
     @Test
     public void testCheckersBuilder() {
+        // given
+        Checkers checkers = new Checkers.CheckersBuilder()
+                .
 
     }
 }
