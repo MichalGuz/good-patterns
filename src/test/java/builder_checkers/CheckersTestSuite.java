962e97ffc006ecd705046e8ec41e90e3c876917e
MichalGuz <michalguz@gmail.com>
2019-10-30 15:07:50 +0100

    Inside 'given' section of testCheckersBuilder(): amend the int arguments in constructor of Figure objects.

--- src/test/java/builder_checkers/CheckersTestSuite.java
+++ src/test/java/builder_checkers/CheckersTestSuite.java
@@ -9,10 +9,11 @@ public class CheckersTestSuite {
         Checkers checkers = new Checkers.CheckersBuilder()
                 .PlayerOne("Kasparov")
                 .PlayerTwo("Anand")
-                .Figure(FigureFactory.PAWN, Figure.WHITE, 0, 3)
-                .Figure(FigureFactory.PAWN, Figure.WHITE, 0, 7)
-                .Figure(FigureFactory.PAWN, Figure.BLACK, 9, 3)
-                .Figure(FigureFactory.PAWN, Figure.BLACK, 9, 7)
+                .Figure(FigureFactory.PAWN, Figure.WHITE, 1, 3)
+                .Figure(FigureFactory.PAWN, Figure.BLACK, 8, 3)
+                .Figure(FigureFactory.QUEEN, Figure.WHITE, 0, 3)
+                .Figure(FigureFactory.QUEEN, Figure.BLACK, 9, 5)
+                .build();
 
     }
 }
