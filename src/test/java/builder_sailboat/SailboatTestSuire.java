f7a43c8137048b9206f42091c964bb9c342287aa
MichalGuz <michalguz@gmail.com>
2019-10-31 22:14:19 +0100

    Create 'given' section of method testSailBoatNew(): create new Sailboat using SailBoatBuilder() and setters from this class.

--- src/test/java/builder_sailboat/SailboatTestSuire.java
+++ src/test/java/builder_sailboat/SailboatTestSuire.java
@@ -5,5 +5,11 @@ import org.junit.Test;
 public class SailboatTestSuire {
     @Test
     public void testSailboatNew() {
+        // given
+        Sailboat sailboat1 = new Sailboat.SailboatBuilder()
+                .setSailboatTyoe("Cutter")
+                .setBoatLenght(6)
+                .setSailsNumber(2)
+
     }
 }
