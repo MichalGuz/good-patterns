fec67057d96b7486e8c24206f53a2c10004844d9
MichalGuz <michalguz@gmail.com>
2019-10-31 21:03:33 +0100

    Create four atributes of class Sailboat.

--- src/main/java/builder_sailboat/Sailboat.java
+++ src/main/java/builder_sailboat/Sailboat.java
@@ -1,4 +1,11 @@
 package builder_sailboat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sailboat {
+    private final String sailboatTyoe;
+    private final int boatLenght;
+    private final int sailsNumber;
+    private List<String> crew = new ArrayList<>();
 }
