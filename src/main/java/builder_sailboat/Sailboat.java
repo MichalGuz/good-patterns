51da09d9138a4ec37a21bb588780ddb5deca6414
MichalGuz <michalguz@gmail.com>
2019-10-31 21:04:00 +0100

    Add 'final' keyword to class Sailboat.

--- src/main/java/builder_sailboat/Sailboat.java
+++ src/main/java/builder_sailboat/Sailboat.java
@@ -3,7 +3,7 @@ package builder_sailboat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Sailboat {
+public final class Sailboat {
     private final String sailboatTyoe;
     private final int boatLenght;
     private final int sailsNumber;
