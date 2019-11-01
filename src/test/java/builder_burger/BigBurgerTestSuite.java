ca46f304aca6efcdfe279393e282746e41bdb3ab
MichalGuz <michalguz@gmail.com>
2019-11-01 11:34:05 +0100

    Create 'given' section of testBigBurgerNew(): create new BigBurger instance using BigBurgerBuilder setters and build() method.

--- src/test/java/builder_burger/BigBurgerTestSuite.java
+++ src/test/java/builder_burger/BigBurgerTestSuite.java
@@ -2,9 +2,23 @@ package builder_burger;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+
 public class BigBurgerTestSuite {
     @Test
     public void testBigBurgerNew() {
-
+        // given
+        BigBurger burger1 = new BigBurger.BigBurgerBuilder()
+                .setBun("Classic")
+                .setBurgerType("Millet groats")
+                .setBurgerDouble(false)
+                .setSauce("Mustard")
+                .setSauce("Spicy tomatoes")
+                .setIngredients("Onion")
+                .setIngredients("Lettuce")
+                .setIngredients("Tomato")
+                .setIngredients("Cucumber")
+                .setIngredients("Capars")
+                .build();
     }
 }
