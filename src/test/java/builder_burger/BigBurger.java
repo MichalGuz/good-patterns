1868b89e006dcfd5293964315471099b8c837b10
MichalGuz <michalguz@gmail.com>
2019-11-01 10:39:46 +0100

    Create atributes of class BigBurger.

--- src/test/java/builder_burger/BigBurger.java
+++ src/test/java/builder_burger/BigBurger.java
@@ -1,4 +1,12 @@
 package builder_burger;
 
+import java.util.List;
+
 public final class BigBurger {
+    private final String bun;
+    private final String burgerType;
+    private final boolean burgerDouble;
+    private List<String> sauce;
+    private List<String> ingredients;
+
 }
