e83a84188465384e5e8b41cebf882911d61a0a70
MichalGuz <michalguz@gmail.com>
2019-11-02 21:03:36 +0100

    Create constructor in class ForumTopic.

--- src/main/java/observer_forum/ForumTopic.java
+++ src/main/java/observer_forum/ForumTopic.java
@@ -1,8 +1,14 @@
 package observer_forum;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ForumTopic {
     private final String name;
     private final List<String> messages;
+
+    public ForumTopic(String name) {
+        this.name = name;
+        this.messages = new ArrayList<>();
+    }
 }
