8dff95a4322d289221113ffb14246d14cdc90b94
MichalGuz <michalguz@gmail.com>
2019-11-05 23:36:23 +0100

    Create constructor of class ForumUser.

--- src/main/java/observer_forum/ForumUser.java
+++ src/main/java/observer_forum/ForumUser.java
@@ -3,4 +3,9 @@ package observer_forum;
 public class ForumUser implements Observer {
     private final String userName;
     private int updatesCount;
+
+    public ForumUser(String userName) {
+        this.userName = userName;
+    }
 }
+
