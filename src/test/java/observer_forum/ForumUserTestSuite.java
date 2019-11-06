5da73d1ab92296be680c4ca314254893ebb918d4
MichalGuz <michalguz@gmail.com>
2019-11-06 23:57:39 +0100

    Invoke method registerObserver() in 'given' section of testUpdate() method.

--- src/test/java/observer_forum/ForumUserTestSuite.java
+++ src/test/java/observer_forum/ForumUserTestSuite.java
@@ -11,5 +11,8 @@ public class ForumUserTestSuite {
         ForumUser martinNovak = new ForumUser("Martin Novak");
         ForumUser adamAdamski = new ForumUser("Adam Adamski");
         ForumUser evaSmith = new ForumUser("Eva Smith");
+        javaHelpForum.registerObserver(martinNovak);
+        javaHelpForum.registerObserver(adamAdamski);
+        javaHelpForum.registerObserver(evaSmith);
     }
 }
