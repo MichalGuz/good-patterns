eeec2d1554f747a1ac642e458c1523b1d94f33bf
MichalGuz <michalguz@gmail.com>
2019-11-06 23:56:19 +0100

    Create 'given' section of testUpdate() method: create new objects of ForumTopic and ForumUser.

--- src/test/java/observer_forum/ForumUserTestSuite.java
+++ src/test/java/observer_forum/ForumUserTestSuite.java
@@ -5,5 +5,11 @@ import org.junit.Test;
 public class ForumUserTestSuite {
     @Test
     public void testUpdate() {
+        // given
+        ForumTopic javaHelpForum = new JavaHelpForumTopic();
+        ForumTopic javaToolForum = new JavaToolsForumTopic();
+        ForumUser martinNovak = new ForumUser("Martin Novak");
+        ForumUser adamAdamski = new ForumUser("Adam Adamski");
+        ForumUser evaSmith = new ForumUser("Eva Smith");
     }
 }
