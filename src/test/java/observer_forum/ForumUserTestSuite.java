0cae03ced0d3f676f22f80ff3545083a2d3d114d
MichalGuz <michalguz@gmail.com>
2019-11-06 23:58:19 +0100

    Create 'when' section of testUpdate() method.

--- src/test/java/observer_forum/ForumUserTestSuite.java
+++ src/test/java/observer_forum/ForumUserTestSuite.java
@@ -14,5 +14,7 @@ public class ForumUserTestSuite {
         javaHelpForum.registerObserver(martinNovak);
         javaHelpForum.registerObserver(adamAdamski);
         javaHelpForum.registerObserver(evaSmith);
+
+        // when
     }
 }
