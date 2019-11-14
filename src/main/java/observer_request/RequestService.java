ff8f8592876bec533d7ae5d6f91f828511bbaa01
MichalGuz <michalguz@gmail.com>
2019-11-14 23:51:00 +0100

    Create constructor of class RequestService.

--- src/main/java/observer_request/RequestService.java
+++ src/main/java/observer_request/RequestService.java
@@ -1,8 +1,14 @@
 package observer_request;
 
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class RequestService {
     private final Queue<String> reqests;
     private final String customersId;
+
+    public RequestService(String customersId) {
+        reqests = new ArrayDeque<>();
+        this.customersId = customersId;
+    }
 }
