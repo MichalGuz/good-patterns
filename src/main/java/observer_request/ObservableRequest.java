c1622f780dc2a4ebe50fcc653ee6a8ed5b751e41
MichalGuz <michalguz@gmail.com>
2019-11-15 00:09:34 +0100

    Create three void methods in ObservableRequest.

--- src/main/java/observer_request/ObservableRequest.java
+++ src/main/java/observer_request/ObservableRequest.java
@@ -1,4 +1,7 @@
 package observer_request;
 
 public interface ObservableRequest {
+    void registerRequestObserver(ObserverRequests observerRequestst);
+    void notifyRequestObserver();
+    void removeRequestObserver(ObserverRequests observerRequests);
 }
