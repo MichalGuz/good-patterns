d761644d582854db712483e1f42ec59507f615ee
MichalGuz <michalguz@gmail.com>
2019-11-16 00:14:12 +0100

    Correct typo in name of variable 'requests' in class RequestService.

--- src/main/java/observer_request/RequestService.java
+++ src/main/java/observer_request/RequestService.java
@@ -6,18 +6,18 @@ import java.util.List;
 import java.util.Queue;
 
 public class RequestService implements ObservableRequests {
-    private final Queue<String> reqests;
+    private final Queue<String> requests;
     private final String customersId;
     private final List<ObserverOfRequests> observers;
 
     public RequestService(String customersId) {
-        reqests = new ArrayDeque<>();
+        requests = new ArrayDeque<>();
         this.customersId = customersId;
         observers = new ArrayList<>();
     }
 
     private void addRequest(String request) {
-        reqests.add(request);
+        requests.add(request);
     }
 
     @Override
