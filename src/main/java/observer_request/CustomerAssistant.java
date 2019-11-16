96958491625c1a77cc21a07d781d722ccc382aa7
MichalGuz <michalguz@gmail.com>
2019-11-16 21:10:50 +0100

    Create constructor of class CustomerAssistant.

--- src/main/java/observer_request/CustomerAssistant.java
+++ src/main/java/observer_request/CustomerAssistant.java
@@ -3,4 +3,8 @@ package observer_request;
 public class CustomerAssistant implements ObserverOfRequests {
     private final String assistantName;
     private int requestCounter;
+
+    public CustomerAssistant(String assistantName) {
+        this.assistantName = assistantName;
+    }
 }
