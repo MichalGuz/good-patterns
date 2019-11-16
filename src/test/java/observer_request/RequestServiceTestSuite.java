fe5f7840c88f77af86d8b46d6607f3a55faf9a12
MichalGuz <michalguz@gmail.com>
2019-11-16 21:34:03 +0100

    Create 'given' section of testUpdate() method in class RequestServiceTestSuite.

--- src/test/java/observer_request/RequestServiceTestSuite.java
+++ src/test/java/observer_request/RequestServiceTestSuite.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 public class RequestServiceTestSuite {
     @Test
     public void testUpdate() {
-
+        // given
+        RequestService company01 = new Company01RequestService();
+        RequestService company10 = new Company10RequestService();
+        RequestService company20 = new Company20RequestService();
     }
 }
