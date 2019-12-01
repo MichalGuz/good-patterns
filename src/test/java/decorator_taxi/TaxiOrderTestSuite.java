cda061b672b8f1d3827b5c44f7421fd75652393f
MichalGuz <michalguz@gmail.com>
2019-12-01 23:51:37 +0100

    Create 'when' section of testBasicTaxiOrderGetCost(): initialize BigDecimal variable  using invocation of getCost() - the TaxiOrder method

--- src/test/java/decorator_taxi/TaxiOrderTestSuite.java
+++ src/test/java/decorator_taxi/TaxiOrderTestSuite.java
@@ -2,10 +2,15 @@ package decorator_taxi;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class TaxiOrderTestSuite {
     @Test
     public void testBasicTaxiOrderGetCost() {
         // given
         TaxiOrder order = new BasicTaxiOrder();
+
+        // when
+        BigDecimal calculatedCost = order.getCost();
     }
 }
