443f1ceb1b6a8c2e086cc641847b96c65f34685a
MichalGuz <michalguz@gmail.com>
2019-12-01 23:49:21 +0100

    Create 'given' section of testBasicTaxiOrderGetCost(): create new object of BasicTaxiOrder using the TaxiOrder interface.

--- src/test/java/decorator_taxi/TaxiOrderTestSuite.java
+++ src/test/java/decorator_taxi/TaxiOrderTestSuite.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 public class TaxiOrderTestSuite {
     @Test
     public void testBasicTaxiOrderGetCost() {
-
+        // given
+        TaxiOrder order = new BasicTaxiOrder();
     }
 }
