9bceed0376c2297a8899b0a4ab754112d052ef83
MichalGuz <michalguz@gmail.com>
2019-12-02 23:59:53 +0100

    Create 'then' section of testCityTaxiNetworkGetCost: invoke assertEquals method.

--- src/test/java/decorator_taxi/TaxiOrderTestSuite.java
+++ src/test/java/decorator_taxi/TaxiOrderTestSuite.java
@@ -39,6 +39,9 @@ public class TaxiOrderTestSuite {
 
         // when
         BigDecimal theCost = taxiOrder.getCost();
+
+        // then
+        Assert.assertEquals(new BigDecimal(40), theCost);
     }
 
     @Test
