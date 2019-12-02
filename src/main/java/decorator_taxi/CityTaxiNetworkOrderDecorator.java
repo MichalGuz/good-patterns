e0415c63c197337c3da5a2e2ce88917b789d36ce
MichalGuz <michalguz@gmail.com>
2019-12-02 23:45:12 +0100

    Override getCost() method in CityTaxiNetworkOrderDecorator class.

--- src/main/java/decorator_taxi/CityTaxiNetworkOrderDecorator.java
+++ src/main/java/decorator_taxi/CityTaxiNetworkOrderDecorator.java
@@ -1,7 +1,15 @@
 package decorator_taxi;
 
+
+import java.math.BigDecimal;
+
 public class CityTaxiNetworkOrderDecorator extends AbstractTaxiOrderDecorator {
     public CityTaxiNetworkOrderDecorator(TaxiOrder taxiOrder) {
         super(taxiOrder);
+    }
+
+    @Override
+    public BigDecimal getCost() {
+        return super.getCost().add(new BigDecimal(40));
     }
 }
