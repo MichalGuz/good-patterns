07bf82c199f3ade25a5038d24bc58c50860bacf4
MichalGuz <michalguz@gmail.com>
2019-12-03 23:09:48 +0100

    Override getCost() method in CiaoTaxiNetworkOrderDecorator.

--- src/main/java/decorator_taxi/CiaoTaxiNetworkOrderDecorator.java
+++ src/main/java/decorator_taxi/CiaoTaxiNetworkOrderDecorator.java
@@ -1,7 +1,15 @@
 package decorator_taxi;
 
+import java.math.BigDecimal;
+
 public class CiaoTaxiNetworkOrderDecorator extends AbstractTaxiOrderDecorator {
     public CiaoTaxiNetworkOrderDecorator (TaxiOrder taxiOrder) {
         super(taxiOrder);
+    }
+
+    @Override
+    public BigDecimal getCost() {
+        // hardcoded stub cost = 20
+        return super.getCost().add(new BigDecimal(20));
     }
 }
