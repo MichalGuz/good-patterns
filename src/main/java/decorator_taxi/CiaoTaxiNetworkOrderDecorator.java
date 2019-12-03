903dae1f5b74b10a7eba898870eda25a7d07378e
MichalGuz <michalguz@gmail.com>
2019-12-03 23:02:50 +0100

    Create constructor of CiaoTaxiNetworkOrderDecorator with super() method inside constructor.

--- src/main/java/decorator_taxi/CiaoTaxiNetworkOrderDecorator.java
+++ src/main/java/decorator_taxi/CiaoTaxiNetworkOrderDecorator.java
@@ -1,4 +1,7 @@
 package decorator_taxi;
 
 public class CiaoTaxiNetworkOrderDecorator extends AbstractTaxiOrderDecorator {
+    public CiaoTaxiNetworkOrderDecorator (TaxiOrder taxiOrder) {
+        super(taxiOrder);
+    }
 }
