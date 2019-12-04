66dfc2bc447c5dabadfe899f5709a83e20fa4350
MichalGuz <michalguz@gmail.com>
2019-12-04 23:10:10 +0100

    Amend the name of ChildSeatDecorator class.

---
+++ src/main/java/decorator_taxi/ChildSeatDecorator.java
@@ -0,0 +1,7 @@
+package decorator_taxi;
+
+public class ChildSeatDecorator extends AbstractTaxiOrderDecorator {
+    public ChildSeatDecorator(TaxiOrder taxiOrder) {
+        super(taxiOrder);
+    }
+}
--- src/main/java/decorator_taxi/ChildseatDecorator.java
+++
@@ -1,7 +0,0 @@
-package decorator_taxi;
-
-public class ChildseatDecorator extends AbstractTaxiOrderDecorator {
-    public ChildseatDecorator(TaxiOrder taxiOrder) {
-        super(taxiOrder);
-    }
-}
