84aef8b313bb4021aedb860f27a8ea0349e67cea
MichalGuz <michalguz@gmail.com>
2019-12-16 23:56:39 +0100

    Create fields of CourierAlfaProduct class.

--- src/main/java/factory_delivery_basic_non_factory/CourierAlfaProduct.java
+++ src/main/java/factory_delivery_basic_non_factory/CourierAlfaProduct.java
@@ -1,4 +1,8 @@
 package factory_delivery_basic_non_factory;
 
 public class CourierAlfaProduct {
+    private String name;
+    private String id;
+    private String sender;
+    private String receiver;
 }
