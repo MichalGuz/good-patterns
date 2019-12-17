d860123507670df63c469ffefe93e824e0f3d7f4
MichalGuz <michalguz@gmail.com>
2019-12-17 23:53:38 +0100

    Create fields of CourierExpressProduct class.

--- src/main/java/factory_delivery_basic_non_factory/CourierExpressProduct.java
+++ src/main/java/factory_delivery_basic_non_factory/CourierExpressProduct.java
@@ -1,5 +1,8 @@
 package factory_delivery_basic_non_factory;
 
 public class CourierExpressProduct implements Product {
-
+    private String name;
+    private String id;
+    private String sender;
+    private String receiver;
 }
