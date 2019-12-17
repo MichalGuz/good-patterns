364246239cc4ed434089ea2f489294346892bf8b
MichalGuz <michalguz@gmail.com>
2019-12-18 00:08:46 +0100

    Create field of class CourierProductMain and its method process().

--- src/main/java/factory_delivery_basic_non_factory/CourierProductMain.java
+++ src/main/java/factory_delivery_basic_non_factory/CourierProductMain.java
@@ -1,4 +1,14 @@
 package factory_delivery_basic_non_factory;
 
 public class CourierProductMain {
+    String type;
+
+    public void process(String type) {
+        Product product;
+        if(type.equals("Alfa")) {
+            product = new CourierAlfaProduct();
+        } else if (type.equals("Express")) {
+            product= new CourierExpressProduct();
+        }
+    }
 }
