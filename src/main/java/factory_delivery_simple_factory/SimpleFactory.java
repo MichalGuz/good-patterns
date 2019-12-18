a1300aa3b8917a1a26bac533c80c1d1a109a16dd
MichalGuz <michalguz@gmail.com>
2019-12-18 22:46:58 +0100

    Create createProduct(String type) method SimpleFactory class.

--- src/main/java/factory_delivery_simple_factory/SimpleFactory.java
+++ src/main/java/factory_delivery_simple_factory/SimpleFactory.java
@@ -1,4 +1,9 @@
 package factory_delivery_simple_factory;
 
+import factory_delivery_basic_non_factory.Product;
+
 public class SimpleFactory {
+    public Product createProduct(String type) {
+
+    }
 }
