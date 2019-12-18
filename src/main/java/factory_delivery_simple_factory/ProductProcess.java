5b7d545e87e0678f1b868c0ac56e513e5f95d3f1
MichalGuz <michalguz@gmail.com>
2019-12-18 22:56:33 +0100

    Create SimpleFactory field and method prodcutProcess(SimpleFactory simpleFactory)inside the ProductProcess class.

--- src/main/java/factory_delivery_simple_factory/ProductProcess.java
+++ src/main/java/factory_delivery_simple_factory/ProductProcess.java
@@ -1,4 +1,7 @@
 package factory_delivery_simple_factory;
 
 public class ProductProcess {
+    private SimpleFactory simpleFactory;
+
+    public ProductProcess productProcess(SimpleFactory simpleFactory) {}
 }
