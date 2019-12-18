0dff7286bd345099c86ba27eed8f2d97398912ae
MichalGuz <michalguz@gmail.com>
2019-12-18 22:58:50 +0100

    Transform the prodcutProcess(SimpleFactory simpleFactory) method into the constructor of the ProductProcess class.

--- src/main/java/factory_delivery_simple_factory/ProductProcess.java
+++ src/main/java/factory_delivery_simple_factory/ProductProcess.java
@@ -3,5 +3,7 @@ package factory_delivery_simple_factory;
 public class ProductProcess {
     private SimpleFactory simpleFactory;
 
-    public ProductProcess productProcess(SimpleFactory simpleFactory) {}
+    public ProductProcess(SimpleFactory simpleFactory) {
+        this.simpleFactory = simpleFactory;
+    }
 }
