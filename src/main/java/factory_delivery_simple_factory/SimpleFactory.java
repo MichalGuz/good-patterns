b3dc98a7e5e91cb8677783ef54fa3215a95ae45d
MichalGuz <michalguz@gmail.com>
2019-12-18 22:50:02 +0100

    Create body of the method createProduct(String type) SimpleFactory class.

--- src/main/java/factory_delivery_simple_factory/SimpleFactory.java
+++ src/main/java/factory_delivery_simple_factory/SimpleFactory.java
@@ -1,9 +1,17 @@
 package factory_delivery_simple_factory;
 
+import factory_delivery_basic_non_factory.CourierAlfaProduct;
+import factory_delivery_basic_non_factory.CourierExpressProduct;
 import factory_delivery_basic_non_factory.Product;
 
 public class SimpleFactory {
     public Product createProduct(String type) {
-
+        Product product = null;
+        if(type.equals("Alfa")) {
+            product = new CourierAlfaProduct();
+        } else if (type.equals("Express")) {
+            product= new CourierExpressProduct();
+        }
+        return product;
     }
 }
