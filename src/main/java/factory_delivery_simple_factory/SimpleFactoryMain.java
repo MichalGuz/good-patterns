e3d8324c116c97e9ffc481ecf15caea4fa1a97fb
MichalGuz <michalguz@gmail.com>
2019-12-18 23:50:03 +0100

    Create main() method in SimpleFactoryMain: create new ProductProcess object and invoke doProcess() method .

--- src/main/java/factory_delivery_simple_factory/SimpleFactoryMain.java
+++ src/main/java/factory_delivery_simple_factory/SimpleFactoryMain.java
@@ -1,4 +1,8 @@
 package factory_delivery_simple_factory;
 
 public class SimpleFactoryMain {
+    public static void main(String[] args) {
+        ProductProcess process = new ProductProcess(new SimpleFactory());
+        process.doProcess("Alfa");
+    }
 }
