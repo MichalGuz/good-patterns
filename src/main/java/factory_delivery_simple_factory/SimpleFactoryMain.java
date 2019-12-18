20b1ad771fae6702a762a284c392e6d0e59c91af
MichalGuz <michalguz@gmail.com>
2019-12-19 00:23:22 +0100

    Add  process.doProcess(Express) in the body of main() method in SimpleFactoryMain.

--- src/main/java/factory_delivery_simple_factory/SimpleFactoryMain.java
+++ src/main/java/factory_delivery_simple_factory/SimpleFactoryMain.java
@@ -4,5 +4,6 @@ public class SimpleFactoryMain {
     public static void main(String[] args) {
         ProductProcess process = new ProductProcess(new SimpleFactory());
         process.doProcess("Alfar");
+        process.doProcess("Express");
     }
 }
