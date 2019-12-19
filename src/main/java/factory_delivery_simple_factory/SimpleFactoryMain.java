8ceed6f6741106f94c1ab9734cc3e72ef1a1c8b1
MichalGuz <michalguz@gmail.com>
2019-12-19 23:59:02 +0100

    Add comments in SimpleFactoryMain class.

--- src/main/java/factory_delivery_simple_factory/SimpleFactoryMain.java
+++ src/main/java/factory_delivery_simple_factory/SimpleFactoryMain.java
@@ -3,7 +3,11 @@ package factory_delivery_simple_factory;
 public class SimpleFactoryMain {
     public static void main(String[] args) {
         ProductProcess process = new ProductProcess(new SimpleFactory());
+
+        // enter wrong name
         process.doProcess("Alfar");
+
+        // enter right name
         process.doProcess("Express");
     }
 }
