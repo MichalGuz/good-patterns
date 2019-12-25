db0cf6f52b6ceaff0928abeb1e30d725a0c958d5
MichalGuz <michalguz@gmail.com>
2019-12-25 23:46:54 +0100

    Remove class StaticFactoryMain

--- src/main/java/factory_delivery_static_factory/StaticFactoryMain.java
+++
@@ -1,9 +0,0 @@
-package factory_delivery_static_factory;
-
-public class StaticFactoryMain {
-    public static void main(String[] args) {
-        ProductProcessor pr = new ProductProcessor();
-        pr.processProductOfStaticFactory("Master");
-        pr.processProductOfStaticFactory("Mater");
-    }
-}
