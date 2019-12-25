9d4d300cb8dc9122775de4d5231fea4291c23b26
MichalGuz <michalguz@gmail.com>
2019-12-25 23:19:52 +0100

    Create main() method in StaticFactoryMain class: invoke processProductOfStaticFactory() method with right type.

--- src/main/java/factory_delivery_static_factory/StaticFactoryMain.java
+++ src/main/java/factory_delivery_static_factory/StaticFactoryMain.java
@@ -1,4 +1,8 @@
 package factory_delivery_static_factory;
 
 public class StaticFactoryMain {
+    public static void main(String[] args) {
+        ProductProcessor pr = new ProductProcessor();
+        pr.processProductOfStaticFactory("Master");
+    }
 }
