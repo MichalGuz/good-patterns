7a87500e7636390296d6d1a1514065489c71b4bd
MichalGuz <michalguz@gmail.com>
2019-12-25 23:13:53 +0100

    Add and implement methods of Product interface in class CourierMasterProduct.

--- src/main/java/factory_delivery_static_factory/CourierMasterProduct.java
+++ src/main/java/factory_delivery_static_factory/CourierMasterProduct.java
@@ -3,4 +3,13 @@ package factory_delivery_static_factory;
 import factory_delivery_basic_non_factory.Product;
 
 public class CourierMasterProduct implements Product {
+    public void initializeDelivery() {
+        System.out.println("Chose the Courier Master delivery.");
+    }
+    public void wrapUpItem() {
+        System.out.println("Prepare a package for Courier Master.");
+    }
+    public void processDelivery() {
+        System.out.println("Process the delivery by Courier Master.");
+    }
 }
