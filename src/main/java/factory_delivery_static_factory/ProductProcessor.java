7738e35135cdd9e9075aa4b952528a8414bec188
MichalGuz <michalguz@gmail.com>
2019-12-25 23:17:52 +0100

    Create void method processProductOfStaticFactory in ProductProcessor class.

--- src/main/java/factory_delivery_static_factory/ProductProcessor.java
+++ src/main/java/factory_delivery_static_factory/ProductProcessor.java
@@ -1,4 +1,12 @@
 package factory_delivery_static_factory;
 
+import factory_delivery_basic_non_factory.Product;
+
 public class ProductProcessor {
+    public void processProductOfStaticFactory(String type) {
+            Product product = StaticFactory.createProduct(type);
+            product.initializeDelivery();
+            product.wrapUpItem();
+            product.processDelivery();
+    }
 }
