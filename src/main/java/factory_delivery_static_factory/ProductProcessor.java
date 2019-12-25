5e5453b4827fb8eb14de04dd73b8f939c1f639fd
MichalGuz <michalguz@gmail.com>
2019-12-25 23:49:34 +0100

    Amend implementation processProductOfStaticFactory: add try-catch-finally block to handle NullPointerException appeared because of using static method without instance with argument of null value.'

--- src/main/java/factory_delivery_static_factory/ProductProcessor.java
+++ src/main/java/factory_delivery_static_factory/ProductProcessor.java
@@ -4,9 +4,15 @@ import factory_delivery_basic_non_factory.Product;
 
 public class ProductProcessor {
     public void processProductOfStaticFactory(String type) {
+        try{
             Product product = StaticFactory.createProduct(type);
             product.initializeDelivery();
             product.wrapUpItem();
             product.processDelivery();
+        } catch (NullPointerException n) {
+            System.out.println("Unknown type of courier company.\nPlease check the name of entered type and try again!");
+        } finally {
+            System.out.println("Factory delivery = Born for deliver! [static fabric]");
+        }
     }
 }
