bb5e0a07b75db31c31b28f47aca706c5a144cd0e
MichalGuz <michalguz@gmail.com>
2019-12-25 23:16:19 +0100

    Create method createProduct() in StaticFactory class.

--- src/main/java/factory_delivery_static_factory/StaticFactory.java
+++ src/main/java/factory_delivery_static_factory/StaticFactory.java
@@ -1,4 +1,15 @@
 package factory_delivery_static_factory;
 
+import factory_delivery_basic_non_factory.Product;
+
 public class StaticFactory {
+    public static Product createProduct(String type) {
+        Product product = null;
+        if (type.equals("Beta")) {
+            product = new CourierBetaProduct();
+        } else if (type.equals("Master")) {
+            product = new CourierMasterProduct();
+        }
+        return product;
+    }
 }
