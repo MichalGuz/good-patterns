b7b315a113b0d9302643b82c4ca368604572a772
MichalGuz <michalguz@gmail.com>
2019-12-25 23:01:29 +0100

    Add and implement methods of Product interface in class CourierBetaProduct.

--- src/main/java/factory_delivery_static_factory/CourierBetaProduct.java
+++ src/main/java/factory_delivery_static_factory/CourierBetaProduct.java
@@ -3,4 +3,15 @@ package factory_delivery_static_factory;
 import factory_delivery_basic_non_factory.Product;
 
 public class CourierBetaProduct implements Product {
+    public void initializeDelivery() {
+        System.out.println("Chose the Courier Beta delivery.");
+    }
+
+    public void wrapUpItem() {
+        System.out.println("Prepare a package for Courier Beta.");
+    }
+
+    public void processDelivery() {
+        System.out.println("Process the delivery by Courier Beta.");
+    }
 }
