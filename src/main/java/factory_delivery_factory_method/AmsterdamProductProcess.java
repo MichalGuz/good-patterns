6f122a13272c9c1c494c9bf3d2586b53e2cd2d6c
MichalGuz <michalguz@gmail.com>
2019-12-26 23:00:25 +0100

    Override createProduct() method in class AmsterdamProductProcess.

--- src/main/java/factory_delivery_factory_method/AmsterdamProductProcess.java
+++ src/main/java/factory_delivery_factory_method/AmsterdamProductProcess.java
@@ -1,4 +1,16 @@
 package factory_delivery_factory_method;
 
-public class AmsterdamProductProcess extends AbstractProductProcessor{
+import factory_delivery_basic_non_factory.Product;
+
+public class AmsterdamProductProcess extends AbstractProductProcessor {
+    @Override
+    protected Product createProduct(String type) {
+        Product product = null;
+        if (type.equals("Beta")){
+            product = new CourierBetaProductAmsterdam();
+        } else if (type.equals("Alpha")) {
+            product = new CourierAlphaProductAmsterdam();
+        }
+        return product;
+    }
 }
