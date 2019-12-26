85e40f68e1ed7d5f248a200d6eeb527846c38a42
MichalGuz <michalguz@gmail.com>
2019-12-26 22:47:46 +0100

    Create methods of CourierAlphaProductVenice class.

--- src/main/java/factory_delivery_factory_method/CourierAlphaProductVenice.java
+++ src/main/java/factory_delivery_factory_method/CourierAlphaProductVenice.java
@@ -3,5 +3,15 @@ package factory_delivery_factory_method;
 import factory_delivery_basic_non_factory.Product;
 
 public class CourierAlphaProductVenice implements Product {
+    public void initializeDelivery() {
+        System.out.println("Chose the Courier Alpha delivery service by boat.");
+    }
 
+    public void wrapUpItem() {
+        System.out.println("Prepare a package for the Courier Alpha delivery service by boat.");
+    }
+
+    public void processDelivery() {
+        System.out.println("Process the Courier Alpha delivery service by boat.");
+    }
 }
