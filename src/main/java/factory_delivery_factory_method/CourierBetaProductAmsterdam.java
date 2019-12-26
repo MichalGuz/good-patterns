8d4efb49fd2eade060ef5462a9926269c550d13b
MichalGuz <michalguz@gmail.com>
2019-12-26 22:42:51 +0100

    Create methods of CourierBetaProductAmsterdam class.

--- src/main/java/factory_delivery_factory_method/CourierBetaProductAmsterdam.java
+++ src/main/java/factory_delivery_factory_method/CourierBetaProductAmsterdam.java
@@ -3,4 +3,15 @@ package factory_delivery_factory_method;
 import factory_delivery_basic_non_factory.Product;
 
 public class CourierBetaProductAmsterdam implements Product {
+    public void initializeDelivery() {
+        System.out.println("Chose the Courier Beta delivery service by bicycle.");
+    }
+
+    public void wrapUpItem() {
+        System.out.println("Prepare a package for the Courier Beta delivery service by bicycle.");
+    }
+
+    public void processDelivery() {
+        System.out.println("Process the Courier Beta delivery service by bicycle.");
+    }
 }
