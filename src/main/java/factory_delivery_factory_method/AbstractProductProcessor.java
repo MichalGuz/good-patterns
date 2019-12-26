0ac6f7d16a804e693a6222133bd7c2c0ee2966b2
MichalGuz <michalguz@gmail.com>
2019-12-26 22:30:46 +0100

    Create protected abstract Product createProduct() method in abstract class AbstractProductProcessor.

--- src/main/java/factory_delivery_factory_method/AbstractProductProcessor.java
+++ src/main/java/factory_delivery_factory_method/AbstractProductProcessor.java
@@ -5,6 +5,8 @@ import factory_delivery_basic_non_factory.Product;
 import static factory_delivery_static_factory.StaticFactory.createProduct;
 
 public abstract class AbstractProductProcessor {
+    protected abstract Product createProduct(String type);
+
     public void process(String type) {
         Product product = createProduct(type);
         product.initializeDelivery();
