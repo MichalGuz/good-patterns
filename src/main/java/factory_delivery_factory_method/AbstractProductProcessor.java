753f6677ec20fd3f9840a274a9b784d98a5202fa
MichalGuz <michalguz@gmail.com>
2019-12-26 22:26:59 +0100

    Implement void process() method: create new Product object using static factory method createProduct().

--- src/main/java/factory_delivery_factory_method/AbstractProductProcessor.java
+++ src/main/java/factory_delivery_factory_method/AbstractProductProcessor.java
@@ -1,5 +1,11 @@
 package factory_delivery_factory_method;
 
+import factory_delivery_basic_non_factory.Product;
+
+import static factory_delivery_static_factory.StaticFactory.createProduct;
+
 public abstract class AbstractProductProcessor {
-    public void process(String type) {}
+    public void process(String type) {
+        Product product = createProduct(type);
+    }
 }
