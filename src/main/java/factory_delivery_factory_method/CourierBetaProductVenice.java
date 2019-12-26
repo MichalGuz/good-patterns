2c36e2a2a4f4ed73bc2f39b27cfd27875dc27643
MichalGuz <michalguz@gmail.com>
2019-12-26 22:40:23 +0100

    Create methods of CourierBetaProductVenice class.

--- src/main/java/factory_delivery_factory_method/CourierBetaProductVenice.java
+++ src/main/java/factory_delivery_factory_method/CourierBetaProductVenice.java
@@ -3,5 +3,16 @@ package factory_delivery_factory_method;
 import factory_delivery_basic_non_factory.Product;
 
 public class CourierBetaProductVenice implements Product {
+    public void initializeDelivery() {
+        System.out.println("Chose the Courier Beta delivery service by boat.");
+    }
+
+    public void wrapUpItem() {
+        System.out.println("Prepare a package for the Courier Beta delivery service by boat.");
+    }
+
+    public void processDelivery() {
+        System.out.println("Process the Courier Beta delivery service by boat.");
+    }
 
 }
