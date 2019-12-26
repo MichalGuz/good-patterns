c5101ac19f37bbd787f03dc8adcfc1f380ca9548
MichalGuz <michalguz@gmail.com>
2019-12-26 22:50:12 +0100

    Create methods of CourierAlphaProductAmsterdam class.

--- src/main/java/factory_delivery_factory_method/CourierAlphaProductAmsterdam.java
+++ src/main/java/factory_delivery_factory_method/CourierAlphaProductAmsterdam.java
@@ -3,4 +3,15 @@ package factory_delivery_factory_method;
 import factory_delivery_basic_non_factory.Product;
 
 public class CourierAlphaProductAmsterdam implements Product {
+    public void initializeDelivery() {
+        System.out.println("Chose the Courier Alpha delivery service by bicycle.");
+    }
+
+    public void wrapUpItem() {
+        System.out.println("Prepare a package for the Courier Alpha delivery service by bicycle.");
+    }
+
+    public void processDelivery() {
+        System.out.println("Process the Courier Alpha delivery service by bicycle.");
+    }
 }
