e0940ba6f00097bd15f485141d3fd5c2602a6795
MichalGuz <michalguz@gmail.com>
2019-12-27 14:42:55 +0100

    Add annotation @Override to methods of classes: CourierAlphaProductAmsterdam and CourierAlphaProductVenice.

--- src/main/java/factory_delivery_factory_method/CourierAlphaProductAmsterdam.java
+++ src/main/java/factory_delivery_factory_method/CourierAlphaProductAmsterdam.java
@@ -3,14 +3,17 @@ package factory_delivery_factory_method;
 import factory_delivery_basic_non_factory.Product;
 
 public class CourierAlphaProductAmsterdam implements Product {
+    @Override
     public void initializeDelivery() {
         System.out.println("Chose the Courier Alpha delivery service by bicycle.");
     }
 
+    @Override
     public void wrapUpItem() {
         System.out.println("Prepare a package for the Courier Alpha delivery service by bicycle.");
     }
 
+    @Override
     public void processDelivery() {
         System.out.println("Process the Courier Alpha delivery service by bicycle.");
     }
