1c484079b2b4234482c1d2a1342b206accdb1c4a
MichalGuz <michalguz@gmail.com>
2019-12-28 15:54:45 +0100

    Delete unusefull import from AbstractProductProcessor class.

--- src/main/java/factory_delivery_factory_method/AbstractProductProcessor.java
+++ src/main/java/factory_delivery_factory_method/AbstractProductProcessor.java
@@ -2,8 +2,6 @@ package factory_delivery_factory_method;
 
 import factory_delivery_basic_non_factory.Product;
 
-//import static factory_delivery_static_factory.StaticFactory.createProduct;
-
 public abstract class AbstractProductProcessor {
     protected abstract Product createProduct(String type);
 
