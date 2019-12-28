22c635e604a8fed24d0ec30f15ca3c46fd4be7e5
MichalGuz <michalguz@gmail.com>
2019-12-28 15:34:56 +0100

    Override createProduct() method in VeniceProductProcess class.

--- src/main/java/factory_delivery_factory_method/VeniceProductProcess.java
+++ src/main/java/factory_delivery_factory_method/VeniceProductProcess.java
@@ -1,6 +1,17 @@
 package factory_delivery_factory_method;
 
-public class VeniceProductProcess extends AbstractProductProcessor {
+import factory_delivery_basic_non_factory.Product;
 
+public class VeniceProductProcess extends AbstractProductProcessor {
+    @Override
+    public Product createProduct(String type) {
+        Product product = null;
+        if(type.equals("Beta")) {
+            product = new CourierBetaProductVenice();
+        } else if (type.equals("Alpha")) {
+            product = new CourierAlphaProductVenice();
+        }
+        return product;
+    }
 }
 
