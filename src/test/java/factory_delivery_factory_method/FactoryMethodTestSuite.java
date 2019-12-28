dc8e4bca2bd40cb21b3886265a73477e5576c3ec
MichalGuz <michalguz@gmail.com>
2019-12-28 15:44:42 +0100

    Create 'given' section of testProcessMethod(): create AbstractProductProcessor variable.

--- src/test/java/factory_delivery_factory_method/FactoryMethodTestSuite.java
+++ src/test/java/factory_delivery_factory_method/FactoryMethodTestSuite.java
@@ -1,10 +1,12 @@
 package factory_delivery_factory_method;
 
+import factory_delivery_basic_non_factory.Product;
 import org.junit.Test;
 
 public class FactoryMethodTestSuite {
     @Test
     public void testProcessMethod() {
-
+        // given
+        AbstractProductProcessor abstractProductProcessor;
     }
 }
