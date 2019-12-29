33359e042acdbec8ab12e507ca5da1db3dee97ea
MichalGuz <michalguz@gmail.com>
2019-12-29 22:17:36 +0100

    DeliveryItemPacket class implements DeliveryItem.

--- src/main/java/factory_delivery_abstract_factory/DeliveryItemPacket.java
+++ src/main/java/factory_delivery_abstract_factory/DeliveryItemPacket.java
@@ -1,4 +1,7 @@
 package factory_delivery_abstract_factory;
 
-public class DeliveryItemPacket {
+public class DeliveryItemPacket implements DeliveryItem {
+    public void prepareDeliveryItem(double weight, int length, int width, int height){
+        System.out.println("Prepare packet. Measurements of packet: \n   - weight: " + weight + " kg\n   - length: " + length + " cm\n   - width: "  + width + " cm\n   - height: " + height + " cm\nPacket is ready.\n");
+    }
 }
