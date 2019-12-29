d8015305ee9c116450ffa4996eca8de986422754
MichalGuz <michalguz@gmail.com>
2019-12-29 21:55:17 +0100

    Create fields of class Delivery.

--- src/main/java/factory_delivery_abstract_factory/Delivery.java
+++ src/main/java/factory_delivery_abstract_factory/Delivery.java
@@ -1,4 +1,9 @@
 package factory_delivery_abstract_factory;
 
 public class Delivery {
+    private String destination;
+    private String idDelivery;
+    private DeliveryItem deliveryItem;
+    private Courier courier;
+
 }
