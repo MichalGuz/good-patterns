e1a37ca58d063d654a82a8dc1700a4a1050ff796
MichalGuz <michalguz@gmail.com>
2019-12-29 22:16:39 +0100

    Amend DeliveryItem interface.

--- src/main/java/factory_delivery_abstract_factory/DeliveryItem.java
+++ src/main/java/factory_delivery_abstract_factory/DeliveryItem.java
@@ -1,5 +1,5 @@
 package factory_delivery_abstract_factory;
 
 public interface DeliveryItem {
-    void prepareDeliveryItem();
+    void prepareDeliveryItem(double weight, int length, int width, int height);
 }
