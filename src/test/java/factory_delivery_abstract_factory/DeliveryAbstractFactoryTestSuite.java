c2aae44737328307a56eb58345f8ae8c1dea0786
MichalGuz <michalguz@gmail.com>
2019-12-30 00:02:53 +0100

    Create 'when' section of testDeliveryFactory().

--- src/test/java/factory_delivery_abstract_factory/DeliveryAbstractFactoryTestSuite.java
+++ src/test/java/factory_delivery_abstract_factory/DeliveryAbstractFactoryTestSuite.java
@@ -7,5 +7,9 @@ public class DeliveryAbstractFactoryTestSuite {
     public void testDeliveryFactory() {
         // given
         Delivery delivery;
+
+        // when
+        delivery = new Delivery("Jasna 10, Warszawa 00-032, Poland", "23991289234", new EnvelopeDeliveryFactory());
+        delivery = new Delivery("Mroczna 7, Wrocław 50-300, Poland", "34123167299", new PacketDeliveryFactory());
     }
 }
