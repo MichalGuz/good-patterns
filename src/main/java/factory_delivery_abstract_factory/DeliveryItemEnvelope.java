961d2658692e333816997457ecbc445aca2c9241
MichalGuz <michalguz@gmail.com>
2019-12-29 22:31:27 +0100

    Create DeliveryItemEnvelope class.

--- src/main/java/factory_delivery_abstract_factory/DeliveryItemEnvelope.java
+++ src/main/java/factory_delivery_abstract_factory/DeliveryItemEnvelope.java
@@ -1,4 +1,7 @@
 package factory_delivery_abstract_factory;
 
-public class DeliveryItemEnvelope {
+public class DeliveryItemEnvelope implements DeliveryItem {
+    public void prepareDeliveryItem(double weight, int length, int width, int height){
+        System.out.println("Prepare envelope. Measurements of envelope:\n   - weight: " + weight + " kg\n   - length: " + length + " cm\n   - width: "  + width + " cm\n   - height: " + height + " cm\nEnvelope is ready.\n");
+    }
 }
