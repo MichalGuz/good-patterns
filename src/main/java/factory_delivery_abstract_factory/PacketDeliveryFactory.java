a1273ac83c1c981429d920681297e625eed4d82f
MichalGuz <michalguz@gmail.com>
2019-12-29 23:33:11 +0100

    PacketDeliveryFactory implements DeliveryFactory interface.

--- src/main/java/factory_delivery_abstract_factory/PacketDeliveryFactory.java
+++ src/main/java/factory_delivery_abstract_factory/PacketDeliveryFactory.java
@@ -1,4 +1,11 @@
 package factory_delivery_abstract_factory;
 
-public class PacketDeliveryFactory {
+public class PacketDeliveryFactory implements DeliveryFactory {
+    public DeliveryItem createDeliveryItem() {
+        return new DeliveryItemPacket();
+    }
+
+    public Courier createCourier() {
+        return new CourierAlpha();
+    }
 }
