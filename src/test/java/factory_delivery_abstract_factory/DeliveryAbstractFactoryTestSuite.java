7c9713ab1b8c8a6c7a9a973a628dc10ad9c5a9b6
MichalGuz <michalguz@gmail.com>
2019-12-29 23:51:46 +0100

    Create testDeliveryFactory() method in DeliveryAbstractFactoryTestSuite.

--- src/test/java/factory_delivery_abstract_factory/DeliveryAbstractFactoryTestSuite.java
+++ src/test/java/factory_delivery_abstract_factory/DeliveryAbstractFactoryTestSuite.java
@@ -1,4 +1,10 @@
 package factory_delivery_abstract_factory;
 
+import org.junit.Test;
+
 public class DeliveryAbstractFactoryTestSuite {
+    @Test
+    public void testDeliveryFactory() {
+
+    }
 }
