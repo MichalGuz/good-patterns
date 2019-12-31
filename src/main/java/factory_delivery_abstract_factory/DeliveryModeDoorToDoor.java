cce643d13e1840c7307d6aa0ec90feeeb916da7e
MichalGuz <michalguz@gmail.com>
2019-12-31 19:09:22 +0100

    Rename 'DeliveryDoorToDoor' class.

--- src/main/java/factory_delivery_abstract_factory/DeliveryModeDoorToDoor.java
+++ src/main/java/factory_delivery_abstract_factory/DeliveryModeDoorToDoor.java
@@ -1,6 +1,6 @@
 package factory_delivery_abstract_factory;
 
-public class DeliveryModeEnvelope implements DeliveryMode {
+public class DeliveryModeDoorToDoor implements DeliveryMode {
     public void prepareDeliveryItem(){
         System.out.println("Prepare envelope.");
     }
