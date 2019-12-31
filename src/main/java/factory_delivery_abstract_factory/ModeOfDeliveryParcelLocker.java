9206ad3311b80cfb008214a8d8e7ca751c132e8c
MichalGuz <michalguz@gmail.com>
2019-12-31 19:11:11 +0100

    Rename 'ModeOfDeliveryParcelLocker' class.

--- src/main/java/factory_delivery_abstract_factory/ModeOfDeliveryParcelLocker.java
+++ src/main/java/factory_delivery_abstract_factory/ModeOfDeliveryParcelLocker.java
@@ -1,6 +1,6 @@
 package factory_delivery_abstract_factory;
 
-public class DeliveryModePacket implements DeliveryMode {
+public class ModeOfDeliveryParcelLocker implements DeliveryMode {
     public void prepareDeliveryItem(){
         System.out.println("Prepare packet.");
     }
--- src/main/java/factory_delivery_abstract_factory/PacketDeliveryFactory.java
+++ src/main/java/factory_delivery_abstract_factory/PacketDeliveryFactory.java
@@ -2,7 +2,7 @@ package factory_delivery_abstract_factory;
 
 public class PacketDeliveryFactory implements DeliveryFactory {
     public DeliveryMode createDeliveryItem() {
-        return new DeliveryModePacket();
+        return new ModeOfDeliveryParcelLocker();
     }
 
     public Courier createCourier() {
