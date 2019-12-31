5c11e8213dddd723edba8a9908a8d1d26ca5ad11
MichalGuz <michalguz@gmail.com>
2019-12-31 19:19:49 +0100

    Change method of DeliveryFactory.

--- src/main/java/factory_delivery_abstract_factory/DeliveryFactory.java
+++ src/main/java/factory_delivery_abstract_factory/DeliveryFactory.java
@@ -1,6 +1,6 @@
 package factory_delivery_abstract_factory;
 
 public interface DeliveryFactory {
-    ModeOfDelivery createDeliveryItem();
+    ModeOfDelivery chooseModeOfDelivery();
     Courier createCourier();
 }
