e9d47b5430e7befbfce5639ec5c59f6bac89d324
MichalGuz <michalguz@gmail.com>
2019-12-31 19:30:46 +0100

    Correct the proper method implementation in DoorToDoorDeliveryFactory.

--- src/main/java/factory_delivery_abstract_factory/DoorToDoorDeliveryFactory.java
+++ src/main/java/factory_delivery_abstract_factory/DoorToDoorDeliveryFactory.java
@@ -2,7 +2,7 @@ package factory_delivery_abstract_factory;
 
 public class DoorToDoorDeliveryFactory implements DeliveryFactory {
     @Override
-    public ModeOfDelivery chooseModeOfDelivery() {
+    public ModeOfDelivery createModeOfDelivery() {
         return new ModeOfDeliveryDoorToDoor();
     }
 
